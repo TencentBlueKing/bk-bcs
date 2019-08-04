f500d1131abb36679668856dcf3cd5afe2a8c01f
bergzhao <13611129507@163.com>
2019-08-04 16:03:42 +0800

    feature: Mesos容器数据基础库; issue #115

--- bcs-common/pkg/discovery/taskgroup.go
+++ bcs-common/pkg/discovery/taskgroup.go
@@ -3,13 +3,11 @@ package discovery
 import (
 	"fmt"
 	"path"
-	"reflect"
 	"strings"
 	"time"
 
 	"bk-bcs/bcs-common/common/blog"
 	"bk-bcs/bcs-common/pkg/meta"
-	"bk-bcs/bcs-common/pkg/queue"
 	"bk-bcs/bcs-common/pkg/reflector"
 	schedtypes "bk-bcs/bcs-common/pkg/scheduler/types"
 	"bk-bcs/bcs-common/pkg/storage"
@@ -28,21 +26,19 @@ type TaskgroupController interface {
 	// List list all taskgroup datas
 	List(selector labels.Selector) (ret []*schedtypes.TaskGroup, err error)
 	// NamespaceList list specified taskgroups by namespace
+	// ns = namespace
 	NamespaceList(ns string, selector labels.Selector) (ret []*schedtypes.TaskGroup, err error)
 	// ApplicationList list specified taskgroups by application name.namespace
+	// ns = namespace
+	// appname = application name
 	ApplicationList(ns, appname string, selector labels.Selector) (ret []*schedtypes.TaskGroup, err error)
 	// Get taskgroup by the specified taskgroupID
+	// taskgroupID, for examples: 0.test-app.defaultGroup.10001.1564385547430065980
 	GetById(taskgroupId string) (*schedtypes.TaskGroup, error)
 	// Get taskgroup by the specified namespace, taskgroup name
+	// ns = namespace
+	// name = taskgroup name, appname-index, for examples: test-app-0
 	GetByName(ns, name string) (*schedtypes.TaskGroup, error)
-	// RegisterTaskgroupQueue register event callback for Taskgroup
-	RegisterTaskgroupQueue(handler queue.Queue)
-}
-
-type taskgroupEvent struct {
-	EventType watch.EventType
-	Old       *schedtypes.TaskGroup
-	Cur       *schedtypes.TaskGroup
 }
 
 //taskgroupController for dataType resource
@@ -52,9 +48,6 @@ type taskgroupController struct {
 	eventStorage storage.Storage      //remote event storage
 	indexer      k8scache.Indexer     //indexer
 	reflector    *reflector.Reflector //reflector list/watch all datas to local memory cache
-
-	eventCh    chan *taskgroupEvent //event for service==>AppSvc
-	eventQueue queue.Queue          //queue for event message
 }
 
 // List list all taskgroup datas
@@ -110,97 +103,7 @@ func (s *taskgroupController) GetByName(ns, name string) (*schedtypes.TaskGroup,
 	return obj.(*schedtypes.TaskGroup), nil
 }
 
-// RegisterAppNodeHandler register event callback for AppNode
-func (s *taskgroupController) RegisterTaskgroupQueue(handler queue.Queue) {
-	s.eventQueue = handler
-	blog.Infof("taskGroup Controller starting backend goroutine for queue handling")
-	go s.handleTaskGroup()
-}
-
-func (s *taskgroupController) taskOnAdd(obj interface{}) {
-	if obj == nil {
-		return
-	}
-	group, ok := obj.(*schedtypes.TaskGroup)
-	if !ok {
-		blog.Errorf("bk-bcs mesos TaskGroup[Pod] plugin AddEvent get error Object data")
-		return
-	}
-
-	e := &taskgroupEvent{
-		EventType: watch.EventAdded,
-		Cur:       group,
-	}
-	blog.V(3).Infof("bk-bcs taskgroup %s/%s trigger Add Event", group.GetNamespace(), group.GetName())
-	s.eventCh <- e
-}
-
-func (s *taskgroupController) taskOnUpdate(old, cur interface{}) {
-	if old == nil || cur == nil {
-		return
-	}
-	//check if old service is different with current one
-	oldGroup, ok := old.(*schedtypes.TaskGroup)
-	curGroup, cok := cur.(*schedtypes.TaskGroup)
-	if !ok || !cok {
-		blog.Errorf("bk-bcs TaskGroup[Pod] plugin AppOnUpdate got error CurrentObject type")
-		return
-	}
-	if reflect.DeepEqual(oldGroup.Taskgroup, curGroup.Taskgroup) {
-		blog.V(3).Infof("bk-bcs TaskGroup %s/%s is nothing different on EventUpdate", curGroup.GetNamespace(), curGroup.GetName())
-		return
-	}
-
-	e := &taskgroupEvent{
-		EventType: watch.EventUpdated,
-		Old:       oldGroup,
-		Cur:       curGroup,
-	}
-	blog.V(3).Infof("bk-bcs taskgroup %s/%s trigger Update Event", curGroup.GetNamespace(), curGroup.GetName())
-	s.eventCh <- e
-}
-
-func (s *taskgroupController) taskOnDelete(obj interface{}) {
-	if obj == nil {
-		return
-	}
-	group, ok := obj.(*schedtypes.TaskGroup)
-	if !ok {
-		blog.Errorf("bk-bcs taskgroup[pod] plugin OnDelete get error Object data")
-		return
-	}
-	e := &taskgroupEvent{
-		EventType: watch.EventDeleted,
-		Old:       nil,
-		Cur:       group,
-	}
-	blog.V(3).Infof("bk-bcs taskgroup %s/%s trigger Delete Event", group.GetNamespace(), group.GetName())
-	s.eventCh <- e
-}
-
-func (s *taskgroupController) handleTaskGroup() {
-	blog.Infof("bk-bcs taskgroup backgroup goroutine starting...")
-	for {
-		select {
-		case <-s.cxt.Done():
-			blog.Infof("bk-bcs TaskGroup[Pod] plugin event goroutine is asked exit.")
-			return
-		case event, ok := <-s.eventCh:
-			if !ok {
-				blog.Infof("bk-bcs TaskGroup event channel broken, ready to exit.")
-				return
-			}
-			//convert TaskGroup to V1.AppNode
-			e := &queue.Event{
-				Type: event.EventType,
-				Data: event.Cur,
-			}
-			s.eventQueue.Push(e)
-		}
-	}
-}
-
-func NewTaskgroupController(hosts []string) (TaskgroupController, error) {
+func NewTaskgroupController(hosts []string, eventHandler reflector.EventInterface) (TaskgroupController, error) {
 	indexers := k8scache.Indexers{
 		meta.NamespaceIndex:   meta.NamespaceIndexFunc,
 		meta.ApplicationIndex: meta.ApplicationIndexFunc,
@@ -235,15 +138,10 @@ func NewTaskgroupController(hosts []string) (TaskgroupController, error) {
 		stopFn:       stopfn,
 		eventStorage: podclient,
 		indexer:      ts,
-		eventCh:      make(chan *taskgroupEvent, 1024),
 	}
-	handler := &reflector.EventHandler{
-		AddFn:    ctl.taskOnAdd,
-		UpdateFn: ctl.taskOnUpdate,
-		DeleteFn: ctl.taskOnDelete,
-	}
+
 	//create reflector
-	ctl.reflector = reflector.NewReflector(fmt.Sprintf("Reflector-%s", zkConfig.Name), ts, listwatcher, time.Second*600, handler)
+	ctl.reflector = reflector.NewReflector(fmt.Sprintf("Reflector-%s", zkConfig.Name), ts, listwatcher, time.Second*600, eventHandler)
 	//sync all data object from remote event storage
 	err = ctl.reflector.ListAllData()
 	if err != nil {
