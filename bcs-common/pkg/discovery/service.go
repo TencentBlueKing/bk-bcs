e3f9e3db492bab9cad7f0f0ad9b13329a15900da
bergzhao <13611129507@163.com>
2019-08-05 12:20:24 +0800

    feature: tencent License; issue #115

--- bcs-common/pkg/discovery/service.go
+++ bcs-common/pkg/discovery/service.go
@@ -1,3 +1,16 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package discovery
 
 import (
@@ -71,7 +84,7 @@ func (s *serviceController) GetByName(ns, name string) (*commtypes.BcsService, e
 
 func NewServiceController(hosts []string, eventHandler reflector.EventInterface) (ServiceController, error) {
 	indexers := k8scache.Indexers{
-		meta.NamespaceIndex:   meta.NamespaceIndexFunc,
+		meta.NamespaceIndex: meta.NamespaceIndexFunc,
 	}
 
 	ts := k8scache.NewIndexer(ServiceObjectKeyFn, indexers)
