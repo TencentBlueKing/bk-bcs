9a7a0ae2fe91133601acb770cf854211f6512c70
abstractmj <abstractmj@qq.com>
2020-03-18 14:36:41 +0800

    feature: add network policy; issue #417

---
+++ bcs-services/bcs-networkpolicy/main.go
@@ -0,0 +1,65 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.,
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under,
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"bk-bcs/bcs-common/common/blog"
+	"bk-bcs/bcs-services/bcs-networkpolicy/options"
+	"bk-bcs/bcs-services/bcs-networkpolicy/server"
+)
+
+func main() {
+	opt := options.New()
+	options.Parse(opt)
+
+	blog.InitLogs(opt.LogConfig)
+
+	blog.V(3).Infof("ServiceConfig %v", opt.ServiceConfig)
+	blog.V(3).Infof("ServerOnlyCertConfig %v", opt.ServerOnlyCertConfig)
+	blog.V(3).Infof("MetricConfig %v", opt.MetricConfig)
+	blog.V(3).Infof("LogConfig %v", opt.LogConfig)
+	blog.V(3).Infof("serviceRegistry %v, kubeMaster %v, kubeconfig %v",
+		opt.ServiceRegistry, opt.KubeMaster, opt.Kubeconfig)
+	blog.V(3).Infof("kubeResyncPeried %v(s), kubeCacheSyncTimeout %v(s), iptablesSyncPeriod %v(s)",
+		opt.KubeResyncPeriod, opt.KubeCacheSyncTimeout, opt.IPTableSyncPeriond)
+
+	stopChan := make(chan struct{})
+
+	svr := server.New(opt)
+	if err := svr.Init(); err != nil {
+		blog.Fatal(err.Error())
+	}
+	if err := svr.Run(stopChan); err != nil {
+		blog.Fatal(err.Error())
+	}
+
+	interupt := make(chan os.Signal, 10)
+	signal.Notify(interupt, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM,
+		syscall.SIGUSR1, syscall.SIGUSR2)
+	for {
+		select {
+		case <-interupt:
+			blog.Infof("Get signal from system. Exit\n")
+			svr.Stop()
+			return
+		case <-stopChan:
+			blog.Infof("server close")
+			svr.Stop()
+			return
+		}
+	}
+}
