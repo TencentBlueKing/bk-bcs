2f966611a9848a956119111b2fde55e57acd5ed3
DeveloperJim <190177081@qq.com>
2020-04-22 20:43:45 +0800

    feature: add authentication plugin, issue #427

--- bcs-services/bcs-gateway-discovery/bkbcs-auth/handler.lua
+++ bcs-services/bcs-gateway-discovery/bkbcs-auth/handler.lua
@@ -1,162 +1,51 @@
-local constants = require "kong.constants"
+--* Tencent is pleased to support the open source community by making Blueking Container Service available.
+-- Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+-- Licensed under the MIT License (the "License"); you may not use this file except
+-- in compliance with the License. You may obtain a copy of the License at
+-- http://opensource.org/licenses/MIT
+-- Unless required by applicable law or agreed to in writing, software distributed under
+-- the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+-- either express or implied. See the License for the specific language governing permissions and
+-- limitations under the License.
+--
 
-
+local basic = require "kong.plugins.base_plugin"
+local userclient = require "kong.plugins.bkbcs-auth.bkbcs.lua"
 local kong = kong
-local type = type
 
-
-local _realm = 'Key realm="' .. _KONG._NAME .. '"'
-
-
-local BKBCSAuthHandler = {}
-
-
+local BKBCSAuthHandler = basic:extend()
 BKBCSAuthHandler.PRIORITY = 1003
 BKBCSAuthHandler.VERSION = "0.1.0"
 
-
-local function load_credential(key)
-  local cred, err = kong.db.keyauth_credentials:select_by_key(key)
-  if not cred then
-    return nil, err
-  end
-
-  return cred, nil, cred.ttl
+function BKBCSAuthHandler:new(name)
+  BKBCSAuthHandler.super.new(self, "bkbcs-auth")
 end
 
-local function do_authentication(conf)
-  if type(conf.key_names) ~= "table" then
-    kong.log.err("no conf.key_names set, aborting plugin execution")
-    return nil, { status = 500, message = "Invalid plugin configuration" }
-  end
-
-  local headers = kong.request.get_headers()
-  local query = kong.request.get_query()
-  local key
-  local body
-
-  -- read in the body if we want to examine POST args
-  if conf.key_in_body then
-    local err
-    body, err = kong.request.get_body()
-
-    if err then
-      kong.log.err("Cannot process request body: ", err)
-      return nil, { status = 400, message = "Cannot process request body" }
-    end
-  end
-
-  -- search in headers & querystring
-  for i = 1, #conf.key_names do
-    local name = conf.key_names[i]
-    local v = headers[name]
-    if not v then
-      -- search in querystring
-      v = query[name]
-    end
-
-    -- search the body, if we asked to
-    if not v and conf.key_in_body then
-      v = body[name]
-    end
-
-    if type(v) == "string" then
-      key = v
-
-      if conf.hide_credentials then
-        query[name] = nil
-        kong.service.request.set_query(query)
-        kong.service.request.clear_header(name)
-
-        if conf.key_in_body then
-          body[name] = nil
-          kong.service.request.set_body(body)
-        end
-      end
-
-      break
-
-    elseif type(v) == "table" then
-      -- duplicate API key
-      return nil, { status = 401, message = "Duplicate API key found" }
-    end
-  end
-
-  -- this request is missing an API key, HTTP 401
-  if not key or key == "" then
-    kong.response.set_header("WWW-Authenticate", _realm)
-    return nil, { status = 401, message = "No API key found in request" }
-  end
-
-  -- retrieve our consumer linked to this API key
-
-  local cache = kong.cache
-
-  local credential_cache_key = kong.db.keyauth_credentials:cache_key(key)
-  local credential, err = cache:get(credential_cache_key, nil, load_credential,
-                                    key)
-  if err then
-    kong.log.err(err)
-    return kong.response.exit(500, {
-      message = "An unexpected error occurred"
-    })
-  end
-
-  -- no credential in DB, for this key, it is invalid, HTTP 401
-  if not credential then
-    return nil, { status = 401, message = "Invalid authentication credentials" }
-  end
-
-  -----------------------------------------
-  -- Success, this request is authenticated
-  -----------------------------------------
-
-  -- retrieve the consumer linked to this API key, to set appropriate headers
-  local consumer_cache_key, consumer
-  consumer_cache_key = kong.db.consumers:cache_key(credential.consumer.id)
-  consumer, err      = cache:get(consumer_cache_key, nil,
-                                 kong.client.load_consumer,
-                                 credential.consumer.id)
-  if err then
-    kong.log.err(err)
-    return nil, { status = 500, message = "An unexpected error occurred" }
-  end
-
-  set_consumer(consumer, credential)
-
-  return true
-end
-
+-- * access authentication before request to Service
 function BKBCSAuthHandler:access(conf)
-  -- check if preflight request and whether it should be authenticated
-  if not conf.run_on_preflight and kong.request.get_method() == "OPTIONS" then
-    return
+  BKBCSAuthHandler.super.access(self, conf)
+  -- ready to creat bkbcs userclient for authentication
+  local userc = userclient:new(conf.bkbcs_auth_endpoints)
+  local err = userc:init(conf)
+  if err then
+    kong.log.err("[bkbcs-auth] init user-manager client with endpoint [", conf.bkbcs_auth_endpoints, "] failed: ", err)
+    -- response internal error
+    return kong.response.exit(500, {message = "An unexpected error occurred when authentication"})
   end
-
-  if conf.anonymous and kong.client.get_credential() then
-    -- we're already authenticated, and we're configured for using anonymous,
-    -- hence we're in a logical OR between auth methods and we're already done.
-    return
+  -- construct request according configuration
+  local req, err = userc:construct_identity(conf, kong.request)
+  if err then
+    kong.log.err("[bkbcs-auth] construct auth request for [", kong.request.get_method(), "]", kong.request.get_path(), ", err:", err)
+    return kong.response.exit(400, {message = "Bad Request: " .. err})
   end
-
-  local ok, err = do_authentication(conf)
+  -- init success, try to anthentication
+  local ok, err = userc:authentication(req)
+  if err then
+    return kong.response.exit(500, {message = "An unexpected error occurred in verify: " .. err})
+  end
   if not ok then
-    if conf.anonymous then
-      -- get anonymous user
-      local consumer_cache_key = kong.db.consumers:cache_key(conf.anonymous)
-      local consumer, err = kong.cache:get(consumer_cache_key, nil,
-                                           kong.client.load_consumer,
-                                           conf.anonymous, true)
-      if err then
-        kong.log.err("failed to load anonymous consumer:", err)
-        return kong.response.exit(500, { message = "An unexpected error occurred" })
-      end
-
-      set_consumer(consumer, nil)
-
-    else
-      return kong.response.exit(err.status, { message = err.message }, err.headers)
-    end
+    kong.log.warn("token is not allow to access to [", kong.request.get_method(), "]", kong.request.get_path())
+    return kong.response.exit(401, {message = "Resource is Unauthorized"})
   end
 end
 
