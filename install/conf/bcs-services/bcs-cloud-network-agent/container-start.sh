c30ff9514a49bf030068ce783bab981ecd1f30c6
DeveloperJim <190177081@qq.com>
2020-07-08 10:33:20 +0800

    chore: fix err for start.sh, issue #508

--- install/conf/bcs-services/bcs-cloud-network-agent/container-start.sh
+++ install/conf/bcs-services/bcs-cloud-network-agent/container-start.sh
@@ -10,7 +10,7 @@ module="bcs-cloud-network-agent"
 cd /data/bcs/${module}
 chmod +x ${module}
 #check configuration render
-if [ $BCS_CONFIG_TYPE == "render" ] then
+if [ $BCS_CONFIG_TYPE == "render" ]; then
   cat ${module}.json.template | envsubst | tee ${module}.json
 fi
 #ready to start
