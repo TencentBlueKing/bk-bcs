a2753acb94dbd3fda7a185c9faa482e8b7547f43
DeveloperJim <190177081@qq.com>
2020-07-09 11:44:35 +0800

    chore: update bcs-client tools, issue #508

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# zookeeper acl config
-export bcs_zk_client_user='bcs'
-export bcs_zk_client_pwd='bcs'
+# zookeeper config
+export bcs_zk_client_user='bcsce'
+export bcs_zk_client_pwd='bcsce@community2019'
 
-# encryption config
-export bcs_encryption_key='abcdefghijklmnopqrstuvwx'
+# encryption config for community edition container release
+export bcs_encryption_key='!@#^&*()1qaz@WSX3ed84RFV'
 
 # cert password config
 export bcs_server_cert_pwd=''
 export bcs_client_cert_pwd=''
-export bcs_license_server_client_cert_pwd=''
+export bcs_license_server_client_cert_pwd=""
 
 # docker registry config
-export bcs_registry_default_user='bcs'
-export bcs_registry_default_pwd='bcs'
+export bcs_registry_default_user='bcsce'
+export bcs_registry_default_pwd='bcsce'
