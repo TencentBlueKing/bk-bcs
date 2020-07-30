4974a629bee42c76fe0c1635c5ead70dbe1cfd79
abstractmj <abstractmj@qq.com>
2020-07-30 15:17:18 +0800

    docs: modify install conf; issue #502

--- install/conf/bcs-services/bcs-cloud-netservice/container-start.sh
+++ install/conf/bcs-services/bcs-cloud-netservice/container-start.sh
@@ -5,11 +5,6 @@ module="bcs-cloud-netservice"
 cd /data/bcs/${module}
 chmod +x ${module}
 
-# check configuration render
-if [ ${BCS_CONFIG_TYPE} == "render" ]; then
-    cat ${module}.json.template | envsubst | tee ${module}.json
-fi
-
 # ready to start
 /data/bcs/${module}/${module} $@
 
