d8b3400789b0252a5ebc51aec8c3a3395def1bbe
DeveloperJim <190177081@qq.com>
2021-01-25 15:03:34 +0800

    fix: metalinter, issue #144

--- install/conf/bcs-services/bcs-gateway-discovery/apisix-start.sh
+++ install/conf/bcs-services/bcs-gateway-discovery/apisix-start.sh
@@ -9,11 +9,19 @@ fi
 
 apisix start
 
+ps -efww | grep nginx
+
 #setting tls certification by json
 if [ "x${bcsSSLJSON}" == "x" ]; then
   echo "lost apisix bkbcs SSL json"
   exit 1
 fi
+
+echo "waiting for apisix initialization....(3s)"
+
+sleep 3
+
+echo "ready to registe api-gateway tls certification..."
 
 curl http://127.0.0.1:8000/apisix/admin/ssl/bkbcs \
   -H"X-API-KEY: ${adminToken}" -X PUT -d@${bcsSSLJSON}
