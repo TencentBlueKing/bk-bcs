ea32a3ef7cc4214140c89d2f25fc5842a44d54d2
DeveloperJim <190177081@qq.com>
2021-01-25 23:26:59 +0800

    fix: container shell for start, issue #0

--- install/conf/bcs-services/bcs-gateway-discovery/apisix-start.sh
+++ install/conf/bcs-services/bcs-gateway-discovery/apisix-start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #check configuration render
-if [ $BCS_CONFIG_TYPE == "render" ]; then
+if [ "x$BCS_CONFIG_TYPE" == "xrender" ]; then
   #apisix configuration
   cd /usr/local/apisix/conf
   cat config.yaml.template | envsubst | tee config.yaml
+  echo ""
 fi
 
 apisix start
 
+pid=`cat /usr/local/apisix/logs/nginx.pid`
 ps -efww | grep nginx
 
 #setting tls certification by json
@@ -17,15 +19,17 @@ if [ "x${bcsSSLJSON}" == "x" ]; then
   exit 1
 fi
 
-echo "waiting for apisix initialization....(3s)"
+echo "\n waiting for apisix initialization....(3s)"
 
 sleep 3
 
 echo "ready to registe api-gateway tls certification..."
 
-curl http://127.0.0.1:8000/apisix/admin/ssl/bkbcs \
+curl -vv http://127.0.0.1:8000/apisix/admin/ssl/bkbcs \
   -H"X-API-KEY: ${adminToken}" -X PUT -d@${bcsSSLJSON}
 
 #signal trap
 echo "waiting for container exit signal~"
-trap "apisix stop" INT QUIT TERM
+trap "apisix stop; echo api-gateway exit; exit" INT QUIT TERM
+
+tail --pid $pid -f /dev/null
