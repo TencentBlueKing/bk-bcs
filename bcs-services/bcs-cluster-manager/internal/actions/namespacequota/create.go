ef875a36ba04fe71227511c2c28df7affae67030
abstractmj <abstractmj@qq.com>
2021-05-27 12:30:43 +0800

    feature: change cluster manager return code to digital, add region to quota info; issue #904

--- bcs-services/bcs-cluster-manager/internal/actions/namespacequota/create.go
+++ bcs-services/bcs-cluster-manager/internal/actions/namespacequota/create.go
@@ -171,6 +171,7 @@ func (ca *CreateAction) createQuotaToStore() error {
 		Namespace:           ca.req.Namespace,
 		FederationClusterID: ca.req.FederationClusterID,
 		ClusterID:           ca.allocatedCluster,
+		Region:              ca.req.Region,
 		ResourceQuota:       ca.req.ResourceQuota,
 		CreateTime:          createTime,
 		UpdateTime:          createTime,
@@ -181,7 +182,7 @@ func (ca *CreateAction) createQuotaToStore() error {
 	return nil
 }
 
-func (ca *CreateAction) setResp(code uint64, msg string) {
+func (ca *CreateAction) setResp(code uint32, msg string) {
 	ca.resp.Code = code
 	ca.resp.Message = msg
 	ca.resp.Result = (code == types.BcsErrClusterManagerSuccess)
