977315ac6984190df89fb98da00035459f1c6388
HubuHito <1214105385@qq.com>
2021-11-11 16:11:16 +0800

    fix: 修复跳转链接失效问题

--- bcs-ui/frontend/src/views/depot/project-image.vue
+++ bcs-ui/frontend/src/views/depot/project-image.vue
@@ -12,12 +12,6 @@
         <div class="biz-content-wrapper" style="padding: 0;" v-bkloading="{ isLoading: isInitLoading, opacity: 0.1 }">
             <template v-if="!isInitLoading">
                 <div class="biz-panel-header biz-project-image-query">
-                    <div class="left" v-if="$INTERNAL">
-                        <bk-button type="primary" :title="$t('手动上传镜像')" @click="goImageDialog">
-                            <i class="bcs-icon bcs-icon-plus"></i>
-                            <span class="text">{{$t('手动上传镜像')}}</span>
-                        </bk-button>
-                    </div>
                     <div class="right">
                         <div class="biz-search-input" style="width: 300px;">
                             <bkbcs-input right-icon="bk-icon icon-search"
@@ -169,13 +163,6 @@
                         repo: item.repo
                     }
                 })
-            },
-
-            /**
-             * 去上传镜像
-             */
-            goImageDialog () {
-                window.open(`${window.DEVOPS_HOST}/console/artifactory/${this.projectCode}/depot/project-image`)
             },
 
             /**
