b15b6a311af3f9001e68d2f558763833c4e5c404
vhwweng <1010382269@qq.com>
2021-12-30 16:02:54 +0800

    feat: 权限对接v3

--- bcs-ui/frontend/src/directives/authority.ts
+++ bcs-ui/frontend/src/directives/authority.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable camelcase */
 /* eslint-disable no-unused-expressions */
 import { DirectiveBinding } from 'vue/types/options'
-import { VueConstructor } from 'vue'
-// import { bus } from '@/common/bus'
+import { VueConstructor, VNode } from 'vue'
+import { bus } from '@/common/bus'
 import bkTooltips from 'bk-magic-vue/lib/directives/tooltips'
-
+import { userPerms, userPermsByAction } from '@/api/base'
 interface IElement extends HTMLElement {
     [prop: string]: any;
 }
@@ -11,20 +13,34 @@ interface IOptions {
     clickable: boolean;
     offset: number[];
     cls: string;
-    data?: any;
+    autoUpdatePerms?: boolean; // 是否在指令更新的时候重新发送权限请求（disablePerms: false时生效）
+    disablePerms?: boolean; // 是否禁用权限请求（完全交个外部控制clickable的值决定状态）
+    resourceName?: string;
+    actionId?: string | string[];
+    permCtx?: {
+        project_id: string; // 项目权限 如果实例无关，可不传
+        cluster_id: string; // 集群权限 如果实例无关，可不传cluster_id
+        name: string; // 命名空间相关权限 如果实例无关，可不传name
+        template_id: string; // 模板集相关权限  果实例无关，可不传template_id
+        resource_type?: string; // 资源类型
+    };
 }
 
 const DEFAULT_OPTIONS: IOptions = {
-    clickable: true,
+    clickable: false,
     offset: [12, 0],
-    cls: 'bcs-cursor-element'
+    cls: 'bcs-cursor-element',
+    disablePerms: false
 }
 
 function init (el: IElement, binding: DirectiveBinding) {
     const parent = el.parentNode
     const options: IOptions = Object.assign({}, DEFAULT_OPTIONS, binding.value)
-    if (options.clickable || !parent) return
+    if (options.clickable || el.dataset.clickable || !parent) return
 
+    if (!el.cloneEl) {
+        el.cloneEl = el.cloneNode(true)
+    }
     // 替换当前节点（为了移除节点的所有事件）
     parent?.replaceChild(el.cloneEl, el)
 
@@ -54,18 +70,36 @@ function init (el: IElement, binding: DirectiveBinding) {
         cloneEl.element = null
         cloneEl.removeEventListener('mousemove', cloneEl.mouseMoveHandler)
     }
-    // cloneEl.clickHandler = function (e) {
-    //     bus.$emit('show-apply-perm-modal', options.data || {})
-    //     e.stopImmediatePropagation()
-    // }
+    cloneEl.clickHandler = async (e: Event) => {
+        e.stopPropagation()
+        const { actionId, permCtx, resourceName } = options
+        if (!actionId || actionId.length === 0) return
+
+        delete permCtx?.resource_type
+        const data = await userPermsByAction({
+            $actionId: Array.isArray(actionId) ? actionId[0] : actionId,
+            perm_ctx: permCtx
+        }).catch(() => ({}))
+        bus.$emit('show-apply-perm-modal', {
+            apply_url: data?.perms?.apply_url,
+            action_list: [
+                {
+                    action_id: actionId,
+                    resource_name: resourceName
+                }
+            ]
+        })
+    }
 
     cloneEl.addEventListener('mouseenter', cloneEl.mouseEnterHandler)
     cloneEl.addEventListener('mouseleave', cloneEl.mouseLeaveHandler)
-    // cloneEl.addEventListener('click', cloneEl.clickHandler)
+    cloneEl.addEventListener('click', cloneEl.clickHandler)
 }
 
 function destroy (el: IElement) {
     const cloneEl = el.cloneEl
+    if (!cloneEl) return
+
     // 还原原始节点
     const parent = cloneEl.parentNode
     parent?.replaceChild(el, el.cloneEl)
@@ -74,23 +108,58 @@ function destroy (el: IElement) {
     cloneEl.removeEventListener('mouseenter', cloneEl.mouseEnterHandler)
     cloneEl.removeEventListener('mousemove', cloneEl.mouseMoveHandler)
     cloneEl.removeEventListener('mouseleave', cloneEl.mouseLeaveHandler)
+    cloneEl.removeEventListener('click', cloneEl.clickHandler)
     cloneEl.element?.remove()
     cloneEl.element = null
-    // cloneEl.removeEventListener('click', cloneEl.clickHandler)
+    delete el.cloneEl
+}
+
+async function updatePerms (el: IElement, binding: DirectiveBinding) {
+    const { actionId = '', permCtx } = binding.value as IOptions
+    const { resource_type, cluster_id, project_id } = permCtx || {}
+    // 校验数据完整性
+    if (!actionId
+        || !resource_type
+        || (resource_type === 'cluster' && !cluster_id)
+        || (resource_type === 'project' && !project_id)) return
+
+    const actionIds = Array.isArray(actionId) ? actionId : [actionId]
+    const data = await userPerms({
+        action_ids: actionIds,
+        perm_ctx: permCtx
+    }).catch(() => ({}))
+    const clickable = actionIds.every(actionId => data?.perms?.[actionId])
+    el.dataset.clickable = clickable ? 'true' : ''
+
+    const cloneBinding = JSON.parse(JSON.stringify(binding))
+    cloneBinding.value.clickable = clickable
+    destroy(el)
+    init(el, cloneBinding)
 }
 
 export default class AuthorityDirective {
     public static install (Vue: VueConstructor) {
         Vue.directive('authority', {
-            bind (el: IElement, binding: DirectiveBinding) {
+            bind (el: IElement, binding: DirectiveBinding, vNode: VNode) {
                 el.cloneEl = el.cloneNode(true) as IElement
+                const { actionId, disablePerms, clickable } = binding.value as IOptions
+                if (actionId && !disablePerms && !clickable) {
+                    updatePerms(el, binding)
+                }
             },
             inserted (el: IElement, binding: DirectiveBinding) {
                 init(el, binding)
             },
             update (el: IElement, binding: DirectiveBinding) {
-                destroy(el)
-                init(el, binding)
+                setTimeout(() => {
+                    const { autoUpdatePerms, disablePerms } = binding.value as IOptions
+                    if (autoUpdatePerms && !disablePerms) {
+                        updatePerms(el, binding)
+                    } else {
+                        destroy(el)
+                        init(el, binding)
+                    }
+                }, 0)
             },
             unbind (el: IElement) {
                 destroy(el)
