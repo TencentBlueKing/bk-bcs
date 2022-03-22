76ef5d473211a1e4d24c8f313b7db449a4427733
jamesge <gejun.coolfriend@gmail.com>
2022-03-22 12:05:24 +0800

    fix: duplicate shared clusters (#1524)

--- bcs-ui/backend/iam/bcs_iam_migration/migrations/0015_bk_bcs_app_202203221049.py
+++ bcs-ui/backend/iam/bcs_iam_migration/migrations/0015_bk_bcs_app_202203221049.py
@@ -12,20 +12,24 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-import mock
-import pytest
+import codecs
+import json
+import os
+
+from django.conf import settings
+from django.db import migrations
+from iam.contrib.iam_migration.migrator import IAMMigrator
 
 
-@pytest.fixture(autouse=True)
-def patch4resource_api():
-    with mock.patch(
-        'backend.iam.open_apis.authentication.IamBasicAuthentication.authenticate', new=lambda *args, **kwargs: None
-    ), mock.patch(
-        'backend.iam.open_apis.providers.utils.get_client_access_token',
-        new=lambda *args, **kwargs: {"access_token": "test"},
-    ), mock.patch(
-        'backend.iam.open_apis.providers.namespace.get_shared_clusters', new=lambda *args, **kwargs: []
-    ), mock.patch(
-        'backend.iam.open_apis.providers.cluster.get_shared_clusters', new=lambda *args, **kwargs: []
-    ):
-        yield
+def forward_func(apps, schema_editor):
+
+    migrator = IAMMigrator(Migration.migration_json)
+    migrator.migrate()
+
+
+class Migration(migrations.Migration):
+    migration_json = "0015_update_cluster_scoped.json"
+
+    dependencies = [('bcs_iam_migration', '0014_bk_bcs_app_202202211624')]
+
+    operations = [migrations.RunPython(forward_func)]
