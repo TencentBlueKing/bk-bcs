5b868e90d386bd6c736506a1a2d39ab82232a636
schnee <suzh9@mail2.sysu.edu.cn>
2022-03-29 17:18:16 +0800

    minor: update subscribe perm validate (#1557)
    * feat: 资源视图接入权限中心 V3
    
    * feat: 资源视图接入权限中心 V3
    
    * feat: 更新 web_annotations 逻辑
    
    * minor: update comment
    
    * fix: resolve #1552 conversations
    
    * minor: ViewPermAction -> DashboardAction
    
    * minor: subscribe update perm validate
    
    * minor: fix F401

--- bcs-ui/backend/dashboard/subscribe/views.py
+++ bcs-ui/backend/dashboard/subscribe/views.py
@@ -18,20 +18,18 @@ from rest_framework.response import Response
 
 from backend.bcs_web.viewsets import SystemViewSet
 from backend.container_service.clusters.base.utils import get_cluster_type
-from backend.dashboard.constants import DashboardAction
 from backend.dashboard.exceptions import ResourceVersionExpired
 from backend.dashboard.subscribe.constants import DEFAULT_SUBSCRIBE_TIMEOUT, K8S_API_GONE_STATUS_CODE
 from backend.dashboard.subscribe.permissions import IsSubscribeable
 from backend.dashboard.subscribe.serializers import FetchResourceWatchResultSLZ
 from backend.dashboard.subscribe.utils import get_native_kind_resource_client, is_native_kind
-from backend.dashboard.viewsets import PermValidateMixin
 from backend.resources.constants import K8sResourceKind
 from backend.resources.custom_object import CustomObject
 from backend.resources.custom_object.formatter import CustomObjectCommonFormatter
 from backend.utils.basic import getitems
 
 
-class SubscribeViewSet(PermValidateMixin, SystemViewSet):
+class SubscribeViewSet(SystemViewSet):
     """ 订阅相关接口，检查 K8S 资源变更情况 """
 
     def get_permissions(self):
@@ -42,7 +40,6 @@ class SubscribeViewSet(PermValidateMixin, SystemViewSet):
         params = self.params_validate(FetchResourceWatchResultSLZ, context={'ctx_cluster': request.ctx_cluster})
 
         res_kind, res_version, namespace = params['kind'], params['resource_version'], params.get('namespace')
-        self._validate_perm(request.user.username, project_id, cluster_id, namespace, DashboardAction.View)
         watch_kwargs = {
             'namespace': namespace,
             'resource_version': res_version,
