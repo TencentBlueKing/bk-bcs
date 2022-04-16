e954291134e0875a09cffe0ebdf88cb562e903af
joelei <thezero12@hotmail.com>
2022-04-16 16:02:01 +0800

    Add: bcs-federated-apiserver for cmd

--- bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver/cmd/bcs-unified-apiserver/main.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver/cmd/bcs-unified-apiserver/main.go
@@ -1,7 +1,34 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
-import "fmt"
+import (
+	goflag "flag"
+	"fmt"
+	"os"
+
+	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-federated-apiserver/cmd/apiserver/app"
+	"github.com/spf13/pflag"
+	_ "go.uber.org/automaxprocs"
+)
 
 func main() {
-	fmt.Println("leijioamin")
+	command := app.NewAggregationCommand()
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
