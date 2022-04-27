b26f0c0a38866fa7fd8fc63b26ec7ed0bb310cb4
okkervil <okkervilriver37@gmail.com>
2022-04-27 17:32:51 +0800

    fix: bcs common bcsapi storage bugfix (#1696)
    * fix: retrieve all data using offset.

--- bcs-common/pkg/bcsapi/test/storage_test.go
+++ bcs-common/pkg/bcsapi/test/storage_test.go
@@ -20,17 +20,18 @@ func TestStorageCli_QueryK8SGameDeployment(t *testing.T) {
 	config := &bcsapi.Config{
 		Hosts:     []string{"xxx:xxx"},
 		TLSConfig: tlsconfig,
+		Gateway:   true,
 	}
 
 	client := bcsapi.NewClient(config)
 	s := client.Storage()
-	gamedeployments, err := s.QueryK8SGameDeployment("xxx")
+	mesosNamespaces, err := s.QueryMesosNamespace("xxx")
 	if err != nil {
 		return
 	}
-	t.Logf("gamedeployments : %v", gamedeployments)
+	t.Logf("mesosNamespaces : %v", mesosNamespaces)
 
-	for _, ns := range gamedeployments {
+	for _, ns := range mesosNamespaces {
 		t.Log(ns)
 	}
 }
