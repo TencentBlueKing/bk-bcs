011a7664f27b0aa856f74bf64bfb769d0b567bb2
bellke <bellkeyang@tencent.com>
2022-05-11 13:05:53 +0800

    feat: fix featureflag unittest (#1754)

--- bcs-ui/backend/tests/container_service/clusters/test_featureflag.py
+++ bcs-ui/backend/tests/container_service/clusters/test_featureflag.py
@@ -70,7 +70,6 @@ from backend.tests.conftest import TEST_SHARED_CLUSTER_ID
                 'TEMPLATESET',
                 'VARIABLE',
                 'HELM',
-                'METRICS',
             },
         ),
         (
