e2b6a2b7eb7c3669ff8f39b5c5898dc7615504f1
abstractmj <abstractmj@qq.com>
2022-05-12 09:45:39 +0800

    feature: add cluster range for clusternet controller

--- bcs-runtime/bcs-k8s/bcs-component/bcs-clusternet-controller/pkg/util/string.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-clusternet-controller/pkg/util/string.go
@@ -13,12 +13,16 @@
 
 package util
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-clusternet-controller/pkg/constant"
+)
 
 // MatchAnnotationsKeyPrefix 匹配annotations中带上了 AnnotationSubscriptionKeyPrefix 的key
 func MatchAnnotationsKeyPrefix(annotations map[string]string) bool {
-	for key, _ := range annotations {
-		if strings.HasPrefix(key, AnnotationSubscriptionKeyPrefix) {
+	for key := range annotations {
+		if strings.HasPrefix(key, constant.AnnotationSubscriptionKeyPrefix) {
 			return true
 		}
 	}
@@ -29,7 +33,7 @@ func MatchAnnotationsKeyPrefix(annotations map[string]string) bool {
 func FindAnnotationsMathKeyPrefix(annotations map[string]string) map[string]string {
 	ret := make(map[string]string)
 	for key, val := range annotations {
-		if strings.HasPrefix(key, AnnotationSubscriptionKeyPrefix) {
+		if strings.HasPrefix(key, constant.AnnotationSubscriptionKeyPrefix) {
 			ret[key] = val
 		}
 	}
