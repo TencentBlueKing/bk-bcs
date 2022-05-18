f3a6d5e2e48cac9e3e89f2b24019f46858fb7076
joelei <thezero12@hotmail.com>
2022-05-18 10:44:06 +0800

    Update: refine License

--- bcs-services/bcs-monitor/pkg/query/empty_clents.go
+++ bcs-services/bcs-monitor/pkg/query/empty_clents.go
@@ -1,3 +1,16 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package query
 
 import (
@@ -14,7 +27,7 @@ import (
 /*
 	此包中实现thanos的metadata，target，rule，exemplar 的proxy接口
 	替换掉query模块中的各种各个用不到的模块
- */
+*/
 
 var NotImplementErr = errors.New("api not implement")
 
