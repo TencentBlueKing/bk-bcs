b9219c5dd35423137c244fcd4bf2dddb711218b9
schneesu <suzh9@mail2.sysu.edu.cn>
2022-06-08 16:41:43 +0800

    minor: replace pkg/component/bcsapi with bcs-common/pkg/bcsapi

--- bcs-services/cluster-resources/pkg/project/client.go
+++ bcs-services/cluster-resources/pkg/project/client.go
@@ -23,11 +23,11 @@ import (
 	"github.com/patrickmn/go-cache"
 	"go-micro.dev/v4/registry"
 
+	bcsapiProj "github.com/Tencent/bk-bcs/bcs-common/pkg/bcsapi/bcsproject"
 	"github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/common/conf"
 	"github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/common/errcode"
 	"github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/common/runmode"
 	"github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/common/runtime"
-	bcsapiProj "github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/component/bcsapi/bcsproject"
 	"github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/discovery"
 	log "github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/logging"
 	"github.com/Tencent/bk-bcs/bcs-services/cluster-resources/pkg/util/errorx"
