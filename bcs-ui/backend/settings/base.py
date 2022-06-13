b5a06567650e175f4a7c1a421a5ff453e68eb059
jamesge <gejun.coolfriend@gmail.com>
2022-06-13 19:28:00 +0800

    minor: render PREFERRED_DOMAINS to index (#1890)

--- bcs-ui/backend/settings/base.py
+++ bcs-ui/backend/settings/base.py
@@ -506,6 +506,9 @@ CLUSTER_TOOLS_REPO_PREFIX = os.environ.get('CLUSTER_TOOLS_REPO_PREFIX')
 # 是否支持使用 Mesos 服务
 SUPPORT_MESOS = os.environ.get("BKAPP_SUPPORT_MESOS", "false")
 
+# 首选域名列表
+PREFERRED_DOMAINS = [domain.strip() for domain in os.environ.get('PREFERRED_DOMAINS', '').split(',') if domain]
+
 try:
     from .base_ext import *  # noqa
 except ImportError as e:
