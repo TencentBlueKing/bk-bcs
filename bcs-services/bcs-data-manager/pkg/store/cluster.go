305c65665f3fced4bc3943d8c3876f29b790522f
lijiaxian <lijiaxian@tom.com>
2022-08-01 16:22:37 +0800

    fix: 修复aggregate慢查询问题 (merge request !147)

--- bcs-services/bcs-data-manager/pkg/store/cluster.go
+++ bcs-services/bcs-data-manager/pkg/store/cluster.go
@@ -277,17 +277,23 @@ func (m *ModelCluster) GetClusterInfo(ctx context.Context,
 		dimension = types.DimensionMinute
 	}
 	clusterMetricsMap := make([]*types.ClusterData, 0)
-
+	metricStartTime := getStartTime(dimension)
 	pipeline := make([]map[string]interface{}, 0)
 	pipeline = append(pipeline,
-		map[string]interface{}{"$unwind": "$metrics"},
 		map[string]interface{}{"$match": map[string]interface{}{
 			ClusterIDKey: request.ClusterID,
 			DimensionKey: dimension,
 			MetricTimeKey: map[string]interface{}{
-				"$gte": primitive.NewDateTimeFromTime(getStartTime(dimension)),
+				"$gte": primitive.NewDateTimeFromTime(metricStartTime),
 			},
-		}}, map[string]interface{}{"$project": map[string]interface{}{
+		}},
+		map[string]interface{}{"$unwind": "$metrics"},
+		map[string]interface{}{"$match": map[string]interface{}{
+			MetricTimeKey: map[string]interface{}{
+				"$gte": primitive.NewDateTimeFromTime(metricStartTime),
+			},
+		}},
+		map[string]interface{}{"$project": map[string]interface{}{
 			"_id":          0,
 			"metrics":      1,
 			"cluster_id":   1,
