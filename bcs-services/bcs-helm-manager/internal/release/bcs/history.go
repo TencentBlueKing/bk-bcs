b638d122e82739ece5289270e2f7f523374e1eac
adevjoe <adevjoe@gmail.com>
2022-08-12 11:26:32 +0800

    fix time format (merge request !195)
    Squash merge branch 'fix/time-format' into 'master'

--- bcs-services/bcs-helm-manager/internal/release/bcs/history.go
+++ bcs-services/bcs-helm-manager/internal/release/bcs/history.go
@@ -15,11 +15,11 @@ package bcs
 import (
 	"context"
 	"sort"
-	"time"
 
 	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
 	"helm.sh/helm/v3/pkg/chartutil"
 
+	"github.com/Tencent/bk-bcs/bcs-services/bcs-helm-manager/internal/common"
 	"github.com/Tencent/bk-bcs/bcs-services/bcs-helm-manager/internal/release"
 )
 
@@ -47,7 +47,7 @@ func (c *cluster) history(ctx context.Context, op release.HelmHistoryOption) ([]
 			Chart:        v.Chart.Name(),
 			ChartVersion: chartVersion,
 			AppVersion:   v.Chart.AppVersion(),
-			UpdateTime:   v.Info.LastDeployed.Local().Format(time.RFC3339),
+			UpdateTime:   v.Info.LastDeployed.Local().Format(common.TimeFormat),
 			Description:  v.Info.Description,
 			Values:       valuesYaml,
 		})
