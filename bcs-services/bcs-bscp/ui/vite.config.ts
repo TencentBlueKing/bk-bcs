3c22709f62966e14fde9ae39ff1ce3c29ba0c17b
israelli <israelli@tencent.com>
2022-12-08 20:17:31 +0800

    bscp-build-pack (merge request !645)
    Squash merge branch 'bscp-build-pack' into 'master'
    feat: pull upstream master

--- bcs-services/bcs-bscp/ui/vite.config.ts
+++ bcs-services/bcs-bscp/ui/vite.config.ts
@@ -1,18 +1,46 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api/c/compapi/v2/cc/': {
-        target: '/',
-        changeOrigin: true,
-      },
-      '/dev/api/v1/': {
-        target: '/',
-        changeOrigin: true,
+const viteHtml = (options?: any) => {
+  return {
+    name: 'vite-plugin-html-transform',
+    transformIndexHtml(html: string) {
+      const reg = /(src|href)="\/static\//gm;
+      html = html.replace(reg, '$1="{{ .BK_STATIC_URL }}/static/');
+      return html;
+    }
+  }
+}
+
+export default defineConfig(({ command, mode }) => {
+  const plugins = [vue()];
+  console.error('defineConfig command', command);
+  if (command === 'build') {
+    plugins.push(viteHtml())
+  }
+
+  return {
+    build: {
+      outDir: "dist",
+      assetsDir: 'static',
+      copyPublicDir: false,
+      target: 'es2015',
+      
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    },
+    plugins,
+    server: {
+      proxy: {
+        '/api/c/compapi/v2/cc/': {
+          target: '/',
+          changeOrigin: true,
+        },
+        '/dev/api/v1/': {
+          target: '/',
+          changeOrigin: true,
+        }
       }
     }
   }
