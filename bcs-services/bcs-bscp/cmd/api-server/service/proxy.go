7ba145f21e8a8588a34a58216f6dc2d864fb33b3
AlkaidChan <362774405@qq.com>
2023-02-13 11:14:17 +0800

    fix bscp upload and download router (merge request !855)
    Squash merge branch 'fix/upload-download-router' into 'master'
    fix bscp upload and download router

--- bcs-services/bcs-bscp/cmd/api-server/service/proxy.go
+++ bcs-services/bcs-bscp/cmd/api-server/service/proxy.go
@@ -96,12 +96,18 @@ func (p *proxy) handler() http.Handler {
 		r.Mount("/", p.cfgSvrMux)
 	})
 
-	// repo 上传 / 下载 API
+	// repo 上传 API
 	r.Route("/api/v1/api/create/content/upload", func(r chi.Router) {
 		r.Use(p.authorizer.UnifiedAuthentication)
 		r.Use(p.setFilter)
-		r.Put("/{biz_id}/app_id/{app_id}", p.repoRevProxy.ServeHTTP)
-		r.Get("/{biz_id}/app_id/{app_id}", p.repoRevProxy.ServeHTTP)
+		r.Put("/biz_id/{biz_id}/app_id/{app_id}", p.repoRevProxy.ServeHTTP)
+	})
+
+	// repo 下载 API
+	r.Route("/api/v1/api/get/content/download", func(r chi.Router) {
+		r.Use(p.authorizer.UnifiedAuthentication)
+		r.Use(p.setFilter)
+		r.Put("/biz_id/{biz_id}/app_id/{app_id}", p.repoRevProxy.ServeHTTP)
 	})
 
 	return r
