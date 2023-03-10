046e3a7363283e52dda3dc94ef1d2e2323fc56b5
johnypeng <johnypeng@canway.net>
2023-03-10 11:51:02 +0800

    feature: 添加分组和hook脚本相关的schema migration操作

--- bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/sql/20230207215606_init_schema_down.sql
+++ bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/sql/20230207215606_init_schema_down.sql
@@ -1,13 +1,15 @@
 /*
 删除初始化migration所创建的所有表
-删除顺序与创建顺序相反，避免出现类似' referenced by a foreign key constraint '的报错
+删除顺序与表依赖顺序相反，避免出现类似' referenced by a foreign key constraint '的报错
 */
 
 drop table if exists `resource_lock`;
 drop table if exists `strategy_set`;
 drop table if exists `published_strategy_history`;
 drop table if exists `current_published_strategy`;
-drop table if exists `strategy`;
+drop table if exists `group_`;
+drop table if exists `group_category`;
+drop table if exists `hook`;
 drop table if exists `released_config_item`;
 drop table if exists `releases`;
 drop table if exists `current_released_instance`;
@@ -18,6 +20,7 @@ drop table if exists `config_item`;
 drop table if exists `commits`;
 drop table if exists `application`;
 drop table if exists `archived_app`;
+drop table if exists `strategy`;
 drop table if exists `id_generator`;
 drop table if exists `sharding_biz`;
 drop table if exists `sharding_db`;
