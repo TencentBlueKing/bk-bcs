468d3a4fb45cabca38a1259943d3b4b49bc1bb0d
luofann <fannluowork@gmail.com>
2023-03-17 10:08:14 +0800

    update: 字体图标改为本地引用的方式

--- bcs-services/bcs-bscp/ui/src/main.ts
+++ bcs-services/bcs-bscp/ui/src/main.ts
@@ -1,13 +1,12 @@
 import { createApp } from 'vue';
 import { pinia } from './store/index'
-import './style.css';
-import App from './App.vue';
 import bkui, { bkTooltips, bkEllipsis } from 'bkui-vue';
 import 'bkui-vue/dist/style.css'
+import './css/style.css';
+import App from './App.vue';
 import router from './router';
 import './utils/login'
 import i18n from './i18n/index';
-import '@tencent/bk-icon-bk_bscp/src/index.css';
 
 const app = createApp(App)
 
