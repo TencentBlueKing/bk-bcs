88d6c42021f6b6845c8dfdcb18f9b7dfcb7920bf
wenxinlee2015 <wenxinlee2015@gmail.com>
2023-05-20 11:07:37 +0800

    Fix: bscp gas_sql

--- bcs-services/bcs-bscp/pkg/dal/dao/group_app_bind.go
+++ bcs-services/bcs-bscp/pkg/dal/dao/group_app_bind.go
@@ -13,6 +13,8 @@ limitations under the License.
 package dao
 
 import (
+	"bytes"
+	"database/sql"
 	"strconv"
 
 	"bscp.io/pkg/criteria/errf"
@@ -149,14 +151,19 @@ func (dao *groupAppDao) Get(kit *kit.Kit, groupID, appID, bizID uint32) (*table.
 		return nil, errf.New(errf.InvalidParameter, "app id is 0")
 	}
 
-	var sqlSentence []string
-	sqlSentence = append(sqlSentence, "SELECT ", table.GroupAppBindColumns.NamedExpr(), " FROM ",
-		table.GroupAppBindTable.Name(), " WHERE group_id = "+strconv.Itoa(int(groupID))+
-			" AND app_id = "+strconv.Itoa(int(appID))+" AND biz_id = "+strconv.Itoa(int(bizID)))
-	sql := filter.SqlJoint(sqlSentence)
+	var sqlBuf bytes.Buffer
+	sqlBuf.WriteString("SELECT ")
+	sqlBuf.WriteString(table.GroupAppBindColumns.NamedExpr())
+	sqlBuf.WriteString(" FROM ")
+	sqlBuf.WriteString(table.GroupAppBindTable.Name())
+	sqlBuf.WriteString(" WHERE group_id = ? AND app_id = ? AND biz_id = ?")
 
 	item := &table.GroupAppBind{}
-	if err := dao.orm.Do(dao.sd.ShardingOne(bizID).DB()).Get(kit.Ctx, item, sql); err != nil {
+	err := dao.orm.Do(dao.sd.ShardingOne(bizID).DB()).Get(kit.Ctx, item, sqlBuf.String(), groupID, appID, bizID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errf.New(errf.NotFound, "group app bind not found")
+		}
 		return nil, err
 	}
 	return item, nil
