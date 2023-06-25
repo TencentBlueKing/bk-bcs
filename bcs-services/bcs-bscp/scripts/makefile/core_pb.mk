0a2b2f0d88484e3aa283dd85181a6c516c8484b0
AlkaidChan <362774405@qq.com>
2023-06-25 20:43:12 +0800

    add makefile to generate python grpc code

--- bcs-services/bcs-bscp/scripts/makefile/core_pb.mk
+++ bcs-services/bcs-bscp/scripts/makefile/core_pb.mk
@@ -3,6 +3,8 @@
 
 PROTO=$(wildcard ./*.proto)
 VERSION=$(shell protoc --version)
+# DIR_NAME=$(shell basename "$(pwd)")
+DIR_NAME=$(notdir $(CURDIR))
 
 OBJ:=$(patsubst %.proto, %.pb.go, $(PROTO))
 
@@ -16,3 +18,8 @@ all:
 
 clean:
 	@rm -f $(OBJ)
+
+py:
+	mkdir -p ../../../../pkg/protocol/python/core/$(DIR_NAME)
+	@echo $(DIR_NAME)
+	python3 -m grpc_tools.protoc --proto_path=. --proto_path=../../../../../ --proto_path=../../../../pkg/thirdparty/protobuf/ --python_out=../../../../pkg/protocol/python/core/$(DIR_NAME) --pyi_out=../../../../pkg/protocol/python/core/$(DIR_NAME) --grpc_python_out=../../../../pkg/protocol/python/core/$(DIR_NAME) *.proto
