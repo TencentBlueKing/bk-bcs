48c50ce95e38c2492637013b83fbd6730bbfab0a
xcming <326658575@qq.com>
2023-06-27 10:29:20 +0800

    fix: fix codecc (merge request !1220)
    Squash merge branch 'final-codecc' into 'master'
    fix: fix codecc

--- bcs-runtime/bcs-k8s/bcs-component/bcs-cluster-autoscaler/bcs-cluster-autoscaler-1.22/core/buffered_autoscaler.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-cluster-autoscaler/bcs-cluster-autoscaler-1.22/core/buffered_autoscaler.go
@@ -478,7 +478,7 @@ func (b *BufferedAutoscaler) doScaleUp(autoscalingContext *contextinternal.Conte
 
 	unschedulablePods = tpu.ClearTPURequests(unschedulablePods)
 
-	// NOTE: move split and append below to separate PodListProcessor
+	// DOTO: move split and append below to separate PodListProcessor
 	// Some unschedulable pods can be waiting for lower priority pods preemption so they have nominated node to run.
 	// Such pods don't require scale up but should be considered during scale down.
 	unschedulablePods, unschedulableWaitingForLowerPriorityPreemption := core_utils.FilterOutExpendableAndSplit(
