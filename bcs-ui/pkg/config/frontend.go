d6633a9c4b4ce58e04568577d654be0f1950f675
ifooth <thezero12@hotmail.com>
2023-07-13 21:33:35 +0800

    Update: bk_iam_app_url -> bk_iam_host (merge request !1278)
    Squash merge branch 'bcs-ui' into 'master'
    Update: bk_iam_app_url -> bk_iam_host

--- bcs-ui/pkg/config/frontend.go
+++ bcs-ui/pkg/config/frontend.go
@@ -20,7 +20,7 @@ type HostConf struct {
 	DevOpsBCSAPIURL       string `yaml:"devops_bcs_api_url"`      // SaaS Backend api 地址
 	DevOpsArtifactoryHost string `yaml:"devops_artifactory_host"` // 制品库地址
 	BKPaaSHost            string `yaml:"bk_paas_host"`            // PaaS 地址
-	BKIAMAppURL           string `yaml:"bk_iam_app_url"`          // 权限中心
+	BKIAMHost             string `yaml:"bk_iam_host"`             // 权限中心
 	BKCCHost              string `yaml:"bk_cc_host"`              // cmdb
 	BKMonitorHost         string `yaml:"bk_monitor_host"`         // 蓝鲸监控
 	BKSREHOST             string `yaml:"bk_sre_host"`             // 申请服务器地址
