e89e72b105b92b4bd4f8f9715a30f9be27550b9f
porterlin <porterlin@tencent.com>
2023-07-26 17:56:37 +0800

    --task=862471215 [bcs-ingress-controller] 支持展示aws global accelerator绑定端口 (merge request !1322)
    Squash merge branch 'feat-aga' into 'master'
    --task=862471215 [bcs-ingress-controller] 支持展示aws global accelerator绑定端口
    
    
    TAPD: --task=862471215

---
+++ bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/cloud/aws/aga_support_test.go
@@ -0,0 +1,137 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package aws
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-network/pkg/common"
+)
+
+func TestSplitPortMappings(t *testing.T) {
+	support := &AgaSupporter{}
+	testCases := []struct {
+		title        string
+		portPairs    []portPair
+		expectedData []*portMapping
+	}{
+		{
+			title: "单个连续Port段",
+			portPairs: []portPair{
+				{
+					CloudPort: 1081,
+					LocalPort: 81,
+				},
+				{
+					CloudPort: 1080,
+					LocalPort: 80,
+				},
+				{
+					CloudPort: 1082,
+					LocalPort: 82,
+				},
+			},
+			expectedData: []*portMapping{
+				{
+					CloudStartPort: 1080,
+					CloudEndPort:   1082,
+					LocalStartPort: 80,
+					LocalEndPort:   82,
+				},
+			},
+		},
+		{
+			title: "多个连续Port段",
+			portPairs: []portPair{
+				{
+					CloudPort: 1081,
+					LocalPort: 81,
+				},
+				{
+					CloudPort: 1080,
+					LocalPort: 80,
+				},
+				{
+					CloudPort: 1082,
+					LocalPort: 82,
+				},
+				// ---
+				{
+					CloudPort: 2091,
+					LocalPort: 91,
+				},
+				{
+					CloudPort: 2090,
+					LocalPort: 90,
+				},
+				{
+					CloudPort: 2092,
+					LocalPort: 92,
+				},
+				// ---
+				{
+					CloudPort: 1085,
+					LocalPort: 85,
+				},
+			},
+			expectedData: []*portMapping{
+				{
+					CloudStartPort: 1080,
+					CloudEndPort:   1082,
+					LocalStartPort: 80,
+					LocalEndPort:   82,
+				},
+				{
+					CloudStartPort: 1085,
+					CloudEndPort:   1085,
+					LocalStartPort: 85,
+					LocalEndPort:   85,
+				},
+				{
+					CloudStartPort: 2090,
+					CloudEndPort:   2092,
+					LocalStartPort: 90,
+					LocalEndPort:   92,
+				},
+			},
+		},
+		{
+			title: "单独端口",
+			portPairs: []portPair{
+				{
+					CloudPort: 1081,
+					LocalPort: 81,
+				},
+			},
+			expectedData: []*portMapping{
+				{
+					CloudStartPort: 1081,
+					CloudEndPort:   1081,
+					LocalStartPort: 81,
+					LocalEndPort:   81,
+				},
+			},
+		},
+	}
+
+	for index, tCase := range testCases {
+		t.Logf("test %d - title: %s", index, tCase.title)
+		portMappings := support.splitPortMappings(tCase.portPairs)
+		if !reflect.DeepEqual(portMappings, tCase.expectedData) {
+			t.Errorf("test %d - title: %s failed, expectedData: %s, result: %s", index, tCase.title,
+				common.ToJsonString(tCase.expectedData), common.ToJsonString(portMappings))
+		}
+	}
+}
---
+++ bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/httpsvr/aga_support.go
@@ -0,0 +1,102 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package httpsvr
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/emicklei/go-restful"
+	"github.com/pkg/errors"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
+	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/metrics"
+)
+
+func (h *HttpServerClient) getPodRelatedAgaEntrance(request *restful.Request, response *restful.Response) {
+	startTime := time.Now()
+	mf := func(status string) {
+		metrics.ReportAPIRequestMetric("aga_entrance", "GET", status, startTime)
+	}
+	if h.AgaSupporter == nil {
+		_, _ = response.Write(CreateResponseData(errors.New("Not init, check if select aws as cloud provider"), "",
+			nil))
+		mf(strconv.Itoa(http.StatusInternalServerError))
+		return
+	}
+	podName := request.QueryParameter("pod_name")
+	podNamespace := request.QueryParameter("pod_namespace")
+	if podName == "" || podNamespace == "" {
+		_, _ = response.Write(CreateResponseData(errors.New("empty parameter: pod_name or pod_namespace"), "", nil))
+		mf(strconv.Itoa(http.StatusInternalServerError))
+		return
+	}
+
+	region := request.QueryParameter("region")
+	if region == "" {
+		blog.V(4).Infof("empty region parameter, use default region ")
+		region = h.Ops.Region
+	}
+
+	blog.V(3).Infof("getPodRelatedAgaEntrance req[pod_name='%s', pod_namespace='%s', region='%s']", podName,
+		podNamespace, region)
+
+	pod := &v1.Pod{}
+	if err := h.Mgr.GetClient().Get(context.TODO(), types.NamespacedName{
+		Namespace: podNamespace,
+		Name:      podName,
+	}, pod); err != nil {
+		err = errors.Wrapf(err, "get pods '%s/%s' failed", podNamespace, podName)
+		blog.Errorf(err.Error())
+		_, _ = response.Write(CreateResponseData(err, "", nil))
+		mf(strconv.Itoa(http.StatusInternalServerError))
+		return
+	}
+
+	// get node from pod's spec
+	node := &v1.Node{}
+	if err := h.Mgr.GetClient().Get(context.TODO(), types.NamespacedName{
+		Name: pod.Spec.NodeName,
+	}, node); err != nil {
+		err = errors.Wrapf(err, "get node '%s' failed", pod.Spec.NodeName)
+		blog.Errorf(err.Error())
+		_, _ = response.Write(CreateResponseData(err, "", nil))
+		mf(strconv.Itoa(http.StatusInternalServerError))
+		return
+	}
+
+	resp, err := h.AgaSupporter.ListCustomRoutingByDefinition(region, getNodeInstanceID(node), pod.Status.PodIP)
+	if err != nil {
+		_, _ = response.Write(CreateResponseData(err, "", nil))
+		mf(strconv.Itoa(http.StatusInternalServerError))
+		return
+	}
+
+	_, _ = response.Write(CreateResponseData(nil, "", resp))
+	mf(strconv.Itoa(http.StatusOK))
+}
+
+// aws中， node.Spec.ProviderID的格式为aws:///<availability_zone>/<instance_id>
+// 如， aws:///us-west-2a/i-0abcdef1234567890
+func getNodeInstanceID(node *v1.Node) string {
+	providerID := node.Spec.ProviderID
+	parts := strings.Split(providerID, "/")
+	instanceID := parts[len(parts)-1]
+	return instanceID
+}
