4c555ba57ca0e600ff3e3fcf8d3b688c619fae55
Joe Lei <thezero12@hotmail.com>
2023-08-10 16:29:33 +0800

    Feat: bscp vaultserver (#2399)
    * vault-server/.gitkeep
    
    * vault-plugin: (#2377)
    
    支持kv存储、公钥上传、创建密钥对（现支持RSA、SM2）、kv安全获取
    
    * Add bscp vaultserver framework (#2381)
    
    * Update: add vault-barrier
    
    * init vault server
    
    * Add: vault server proto
    
    * Update: add vault-barrier make
    
    * Update: finish vault server
    
    * Update: finish vault vault-barrier
    
    * Update: fix vault-barrier panic
    
    * feature: add bscp vault-server (#2386)
    
    * vault-plugin:
    - 补充错误返回
    - 增加获取kv列表
    - 增加参数校验
    
    * vault-plugin:
    - 修复bug
    
    * Update: update go mod
    
    * Update: update go mod
    
    * Update: fix codecc style
    
    * - 优化key存储路径，apps/{app_id}/keys/{name} => apps/{app_id}/keys/{name}/algorithm (#2405)
    
    - 优化rsa、sm2 封装
    - 提供一个较为完整到单元测试
    - 降低圈复杂度
    - 解决代码冲突
    
    * - 降低圈复杂度 (#2414)
    
    ---------
    
    Co-authored-by: linfang-canway <86582008+linfang-canway@users.noreply.github.com>

---
+++ bcs-services/bcs-bscp/cmd/vault-server/app/app.go
@@ -0,0 +1,264 @@
+/*
+Tencent is pleased to support the open source community by making Basic Service Configuration Platform available.
+Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except
+in compliance with the License. You may obtain a copy of the License at
+http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package app NOTES
+package app
+
+import (
+	"fmt"
+	"math"
+	"net"
+	"net/http"
+	"strconv"
+
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"bscp.io/cmd/vault-server/options"
+	"bscp.io/cmd/vault-server/service"
+	"bscp.io/pkg/cc"
+	"bscp.io/pkg/criteria/uuid"
+	"bscp.io/pkg/logs"
+	"bscp.io/pkg/metrics"
+	pbvs "bscp.io/pkg/protocol/vault-server"
+	"bscp.io/pkg/runtime/brpc"
+	"bscp.io/pkg/runtime/shutdown"
+	"bscp.io/pkg/serviced"
+	"bscp.io/pkg/tools"
+)
+
+// Run start the config server
+func Run(opt *options.Option) error {
+	as := new(vaultService)
+	if err := as.prepare(opt); err != nil {
+		return err
+	}
+
+	if err := as.listenAndServe(); err != nil {
+		return err
+	}
+
+	if err := as.gwListenAndServe(); err != nil {
+		return err
+	}
+
+	if err := as.register(); err != nil {
+		return err
+	}
+
+	shutdown.RegisterFirstShutdown(as.finalizer)
+	shutdown.WaitShutdown(20)
+	return nil
+}
+
+type vaultService struct {
+	serve   *grpc.Server
+	gwServe *http.Server
+	service *service.Service
+	sd      serviced.ServiceDiscover
+}
+
+// prepare do prepare jobs before run config server.
+func (as *vaultService) prepare(opt *options.Option) error {
+	// load settings from config file.
+	if err := cc.LoadSettings(opt.Sys); err != nil {
+		return fmt.Errorf("load settings from config files failed, err: %v", err)
+	}
+
+	logs.InitLogger(cc.VaultServer().Log.Logs())
+
+	logs.Infof("load settings from config file success.")
+
+	// init metrics
+	metrics.InitMetrics(net.JoinHostPort(cc.VaultServer().Network.BindIP,
+		strconv.Itoa(int(cc.VaultServer().Network.RpcPort))))
+
+	etcdOpt, err := cc.VaultServer().Service.Etcd.ToConfig()
+	if err != nil {
+		return fmt.Errorf("get etcd config failed, err: %v", err)
+	}
+
+	// register vault server.
+	svcOpt := serviced.ServiceOption{
+		Name: cc.VaultServerName,
+		IP:   cc.VaultServer().Network.BindIP,
+		Port: cc.VaultServer().Network.RpcPort,
+		Uid:  uuid.UUID(),
+	}
+	sd, err := serviced.NewServiceD(etcdOpt, svcOpt)
+	if err != nil {
+		return fmt.Errorf("new service discovery faield, err: %v", err)
+	}
+
+	as.sd = sd
+	logs.Infof("create service discovery success.")
+
+	return nil
+}
+
+// listenAndServe listen the grpc serve and set up the shutdown gracefully job.
+func (as *vaultService) listenAndServe() error {
+	// generate standard grpc server grpcMetrics.
+	grpcMetrics := grpc_prometheus.NewServerMetrics()
+	grpcMetrics.EnableHandlingTimeHistogram(metrics.GrpcBuckets)
+
+	recoveryOpt := grpc_recovery.WithRecoveryHandlerContext(brpc.RecoveryHandlerFuncContext)
+
+	opts := []grpc.ServerOption{grpc.MaxRecvMsgSize(math.MaxInt32),
+		// add bscp unary interceptor and standard grpc server metrics interceptor.
+		grpc.ChainUnaryInterceptor(
+			brpc.LogUnaryServerInterceptor(),
+			grpcMetrics.UnaryServerInterceptor(),
+			grpc_recovery.UnaryServerInterceptor(recoveryOpt),
+		),
+		grpc.ChainStreamInterceptor(
+			grpcMetrics.StreamServerInterceptor(),
+			grpc_recovery.StreamServerInterceptor(recoveryOpt),
+		),
+	}
+	network := cc.VaultServer().Network
+	if network.TLS.Enable() {
+		tls := network.TLS
+		tlsC, err := tools.ClientTLSConfVerify(tls.InsecureSkipVerify, tls.CAFile, tls.CertFile, tls.KeyFile,
+			tls.Password)
+		if err != nil {
+			return fmt.Errorf("init tls config failed, err: %v", err)
+		}
+
+		cred := credentials.NewTLS(tlsC)
+		opts = append(opts, grpc.Creds(cred))
+
+	}
+
+	serve := grpc.NewServer(opts...)
+	svc, err := service.NewService(as.sd)
+	if err != nil {
+		return fmt.Errorf("initialize service failed, err: %v", err)
+	}
+	pbvs.RegisterVaultServer(serve, svc)
+
+	// initialize and register standard grpc server grpcMetrics.
+	grpcMetrics.InitializeMetrics(serve)
+	if err = metrics.Register().Register(grpcMetrics); err != nil {
+		return fmt.Errorf("register metrics failed, err: %v", err)
+	}
+
+	as.service = svc
+	as.serve = serve
+
+	go func() {
+		notifier := shutdown.AddNotifier()
+		select {
+		case <-notifier.Signal:
+			logs.Infof("start shutdown vault server grpc server gracefully...")
+
+			as.serve.GracefulStop()
+			notifier.Done()
+
+			logs.Infof("shutdown vault server grpc server success...")
+
+		}
+	}()
+
+	addr := net.JoinHostPort(network.BindIP, strconv.Itoa(int(network.RpcPort)))
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("lisen addr: %s failed, err: %v", addr, err)
+	}
+
+	go func() {
+		if err := serve.Serve(listener); err != nil {
+			logs.Errorf("serve grpc server failed, err: %v", err)
+			shutdown.SignalShutdownGracefully()
+		}
+	}()
+
+	logs.Infof("listen grpc server at %s now.", addr)
+
+	return nil
+}
+
+// gwListenAndServe listen the http serve and set up the shutdown gracefully job.
+func (as *vaultService) gwListenAndServe() error {
+	network := cc.VaultServer().Network
+	addr := net.JoinHostPort(network.BindIP, strconv.Itoa(int(network.HttpPort)))
+
+	handler, err := as.service.Handler()
+	if err != nil {
+		return err
+	}
+	as.gwServe = &http.Server{Addr: addr, Handler: handler}
+
+	go func() {
+		notifier := shutdown.AddNotifier()
+		select {
+		case <-notifier.Signal:
+			logs.Infof("start shutdown vault server http server gracefully...")
+
+			as.gwServe.Close()
+			notifier.Done()
+
+			logs.Infof("shutdown vault server http server success...")
+		}
+	}()
+
+	if network.TLS.Enable() {
+		tls := network.TLS
+		tlsC, err := tools.ClientTLSConfVerify(tls.InsecureSkipVerify, tls.CAFile, tls.CertFile, tls.KeyFile,
+			tls.Password)
+		if err != nil {
+			return fmt.Errorf("init tls config failed, err: %v", err)
+		}
+
+		as.gwServe.TLSConfig = tlsC
+
+		go func() {
+			if err := as.gwServe.ListenAndServeTLS("", ""); err != nil {
+				logs.Errorf("gateway https server listen and serve failed, err: %v", err)
+				shutdown.SignalShutdownGracefully()
+			}
+		}()
+
+	} else {
+		go func() {
+			if err := as.gwServe.ListenAndServe(); err != nil {
+				logs.Errorf("gateway http server listen and serve failed, err: %v", err)
+				shutdown.SignalShutdownGracefully()
+			}
+		}()
+	}
+
+	logs.Infof("listen gateway server at %s now.", addr)
+
+	return nil
+}
+
+func (as *vaultService) finalizer() {
+	if err := as.sd.Deregister(); err != nil {
+		logs.Errorf("process service shutdown, but deregister failed, err: %v", err)
+		return
+	}
+
+	logs.Infof("shutting down service, deregister service success.")
+}
+
+// register the grpc serve.
+func (as *vaultService) register() error {
+	if err := as.sd.Register(); err != nil {
+		return fmt.Errorf("register vault server failed, err: %v", err)
+	}
+
+	logs.Infof("register vault server to etcd success.")
+	return nil
+}
---
+++ bcs-services/bcs-bscp/cmd/vault-server/vault-barrier/main.go
@@ -0,0 +1,35 @@
+/*
+Tencent is pleased to support the open source community by making Basic Service Configuration Platform available.
+Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except
+in compliance with the License. You may obtain a copy of the License at
+http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "as IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/vault/command"
+)
+
+func main() {
+	args := []string{
+		"server",
+		"-dev",
+		"-dev-root-token-id=root",
+		"-dev-plugin-dir=./vault/plugins", // 指定插件目录
+	}
+
+	// 可以启动自定义命令
+	if len(os.Args) > 1 {
+		args = os.Args[1:]
+	}
+
+	os.Exit(command.Run(args))
+}
