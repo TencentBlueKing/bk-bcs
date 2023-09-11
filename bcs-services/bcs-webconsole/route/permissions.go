b88e186e7caf1caedc208621e819d787a20917ce
jonesyang12 <130545229+jonesyang12@users.noreply.github.com>
2023-09-11 19:22:36 +0800

    Feat: bcs-webconsole add terminal record (#2521)
    * 解决冲突
    
    * 文件回放
    
    * 子命令替换参数
    
    * web页面回放
    
    * 文件名称格式修改
    
    * detail
    
    * 文件限制100M
    
    * io限制
    
    * 解决冲突2
    
    * 注释/中间件/页面渲染优化
    
    * 定时写入/新增播放页面/优雅关闭
    
    * xg
    
    * upgrade player
    
    * 定时任务bugfix
    
    * routerPrefix
    
    * 目录结构,文件header信息
    
    * 配置文件,中间件调整
    
    * buff修改/新增事件函数/初始化修改/内聚err
    
    * 包名修改
    
    * routerPrefix

--- bcs-services/bcs-webconsole/route/permissions.go
+++ bcs-services/bcs-webconsole/route/permissions.go
@@ -213,3 +213,24 @@ func CredentialRequired() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+// ManagersRequired 校验用户是否为manager
+func ManagersRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authCtx := MustGetAuthContext(c)
+		user := authCtx.Username
+		managers := config.G.Base.Managers
+		for _, manager := range managers {
+			if user == manager {
+				c.Next()
+				return
+			}
+		}
+		c.AbortWithStatusJSON(http.StatusForbidden, types.APIResponse{
+			Code:      types.ApiErrorCode,
+			Message:   fmt.Sprintf("%s is not managers, has no permission", user),
+			RequestID: authCtx.RequestId,
+		})
+		return
+	}
+}
