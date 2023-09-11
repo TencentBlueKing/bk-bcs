9dff49f3430bea955cec543ea2934455083e7c15
root <user.name>
2023-09-11 18:08:16 +0800

    f-bcs-netservice-controller-charts (merge request !1411)
    Squash merge branch 'f-bcs-netservice-controller-charts' into 'master'
    feature: optimize bcs-netservice-controller and bcs-k8s-custom-scheduler logic for fixed ip; issue #2459
    
    * 将固定IPclaim绑定IP的过程放到第一次cni申请ip的时候
    * custom-scheduler在内存中实现预扣除
    * 自动给pool中的节点添加扩展资源

--- bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/app/api-init.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/app/api-init.go
@@ -16,6 +16,7 @@ package app
 import (
 	// import predicate actions
 	_ "github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/pkg/actions/bind"
+	_ "github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/pkg/actions/prebind"
 	_ "github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/pkg/actions/predicate"
 	_ "github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/pkg/metrics"
 )
---
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-k8s-custom-scheduler/pkg/ipscheduler/v3/eventhandler.go
@@ -0,0 +1,91 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under,
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v3
+
+import (
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
+)
+
+type ipHandler struct {
+	cache *PoolCache
+}
+
+func newIPHander(cache *PoolCache) *ipHandler {
+	return &ipHandler{
+		cache: cache,
+	}
+}
+
+// OnAdd implements event handler
+func (ih *ipHandler) OnAdd(obj interface{}) {
+	addUnstruct, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		blog.Warnf("added object %v is not unstructured", obj)
+		return
+	}
+	ip := &BCSNetIP{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(
+		addUnstruct.UnstructuredContent(), ip); err != nil {
+		blog.Warnf("failed to convert unstructured ip %v", addUnstruct)
+		return
+	}
+	syncCachedPoolByIP(ip)
+}
+
+// OnUpdate implements event handler
+func (ih *ipHandler) OnUpdate(oldObj, obj interface{}) {
+	newUnstruct, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		blog.Warnf("new object %v is not unstructured", obj)
+		return
+	}
+	ip := &BCSNetIP{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(
+		newUnstruct.UnstructuredContent(), ip); err != nil {
+		blog.Warnf("failed to convert unstructured ip %v", newUnstruct)
+		return
+	}
+	syncCachedPoolByIP(ip)
+}
+
+// OnDelete implements event handler
+func (ih *ipHandler) OnDelete(obj interface{}) {
+	var ok bool
+	var ipStruct *unstructured.Unstructured
+	var tombstone cache.DeletedFinalStateUnknown
+	ipStruct, ok = obj.(*unstructured.Unstructured)
+	if !ok {
+		tombstone, ok = obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			blog.Warnf("error decoding object, invalid type")
+			return
+		}
+		ipStruct, ok = tombstone.Obj.(*unstructured.Unstructured)
+		if !ok {
+			blog.Warnf("error decoding object tombstone, invalid type")
+			return
+		}
+		blog.Infof("recovered deleted ip '%s/%s' from tombstone", ipStruct.GetName())
+	}
+	ip := &BCSNetIP{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(
+		ipStruct.UnstructuredContent(), ip); err != nil {
+		blog.Warnf("failed to convert unstructured ip %v", ipStruct)
+		return
+	}
+	syncCachedPoolByIP(ip)
+}
