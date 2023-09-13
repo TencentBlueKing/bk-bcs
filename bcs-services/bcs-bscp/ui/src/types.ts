e2e28b808443e079a117c97578f9db43362cb010
Yuikill <1191184301@qq.com>
2023-09-13 14:13:35 +0800

    fix: 修复选择载入脚本无效问题

--- bcs-services/bcs-bscp/ui/src/types.ts
+++ bcs-services/bcs-bscp/ui/src/types.ts
@@ -1,32 +1,117 @@
-export type IPageFilter = {
-  count: Boolean,
-  start: number,
-  limit: number
+export const enum EScriptType {
+	Shell = 'shell',
+	Python = 'python',
 }
 
-export const enum FilterOp {
-  AND = 'and',
-  OR = 'or'
+// 脚本列表查询请求参数
+export interface IScriptListQuery {
+	start: number
+	limit?: number
+	all?: boolean
+	tag?: string
+	not_tag?: boolean
+	name?: string
 }
 
-export const enum RuleOp {
-  eq = 'eq',
-  neq = 'neq',
-  gt = 'gt',
-  gte = 'gte',
-  lt = 'lt',
-  lte = 'lte',
-  in = 'in',
-  nin = 'nin'
+// 新建脚本编辑参数
+export interface IScriptEditingForm {
+	name: string
+	tag: string
+	memo: string
+	type: EScriptType
+	content: string
 }
 
-export type IRequestFilter = {
-  op: FilterOp,
-  rules: IRequestFilterRule[],
+// 脚本
+export interface IScriptItem {
+	bound_num: number
+	confirm_delete: boolean
+	hook: {
+		id: number
+		spec: {
+			name: string
+			type: string
+			tag: string
+			publish_num: number
+			momo: string
+		}
+		attachment: {
+			biz_id: number
+		}
+		revision: {
+			creator: string
+			reviser: string
+			create_at: string
+			update_at: string
+		}
+	}
+	published_revision_id: number
 }
 
-export type IRequestFilterRule = {
-  field: string,
-  op: RuleOp,
-  value: any
+// 脚本标签
+export interface IScriptTagItem {
+	tag: string
+	counts: number
+}
+
+// 脚本版本
+export interface IScriptVersion {
+	id: number
+	spec: {
+		name: string
+		content: string
+		publish_num: number
+		state: string
+		memo: string
+	}
+	attachment: {
+		biz_id: number
+		hook_id: number
+	}
+	revision: {
+		creator: string
+		reviser: string
+		create_at: string
+		update_at: string
+	}
+}
+
+// 脚本版本列表项
+export interface IScriptVersionListItem {
+	bound_num: number
+	confirm_delete: boolean
+	hook_revision: IScriptVersion
+}
+
+// 脚本版本新建、编辑表单
+export interface IScriptVersionForm {
+	id: number
+	name: string
+	memo: string
+	content: string
+}
+
+// 脚本被引用列表查询参数
+export interface IScriptCiteQuery {
+	start: number
+	limit: number
+	release_id?: number
+	searchKey?: string
+}
+
+// 脚本被引用数据
+export interface IScriptCitedItem {
+	hook_release_name: string
+	app_id: number
+	app_name: string
+	config_release_name: string
+	config_release_id: number
+	pub_state: string
+}
+
+//载入脚本加工后列表项
+export interface IScriptMapItem {
+	id: number
+	name: string
+	content: string
 }
--- bcs-services/bcs-bscp/ui/vite.config.ts
+++ bcs-services/bcs-bscp/ui/vite.config.ts
@@ -1,76 +1,79 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import viteCompression from "vite-plugin-compression"
+import viteCompression from 'vite-plugin-compression'
 
 const viteHtml = (options?: any) => {
-  return {
-    name: 'vite-plugin-html-transform',
-    transformIndexHtml(html: string) {
-      const reg = /(src|href)="\.\/static\//gm;
-      html = html.replace(reg, '$1="{{ .BK_STATIC_URL }}/static/');
-      return html;
-    }
-  }
+	return {
+		name: 'vite-plugin-html-transform',
+		transformIndexHtml(html: string) {
+			const reg = /(src|href)="\.\/static\//gm
+			html = html.replace(reg, '$1="{{ .BK_STATIC_URL }}/static/')
+			return html
+		},
+	}
 }
 
 export default defineConfig(({ command, mode }) => {
-  const plugins = [
-    vue(),
-    viteCompression({
-      filter: /\.js|.css$/,
-      threshold: 1
-    })
-  ];
-  console.error('defineConfig command', command);
-  if (command === 'build') {
-    plugins.push(viteHtml())
-  } else {
-    plugins.push(basicSsl())
-  }
+	const plugins = [
+		vue(),
+		viteCompression({
+			filter: /\.js|.css$/,
+			threshold: 1,
+		}),
+	]
+	console.error('defineConfig command', command)
+	if (command === 'build') {
+		plugins.push(viteHtml())
+	}
+	// else {
+	// 	plugins.push(basicSsl())
+	// }
 
-  return {
-    base: "./",
-    publicDir: 'static',
-    plugins,
-    resolve: {
-      alias: {
-        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
-      },
-    },
-    build: {
-      outDir: "dist",
-      assetsDir: 'static',
-      target: 'es2015',
-      commonjsOptions: {
-        transformMixedEsModules: true
-      },
-      rollupOptions: {
-        output: {
-          entryFileNames: 'static/js/[name]-[hash].js',
-          chunkFileNames: 'static/js/[name]-[hash].js',
-          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
-        }
-      }
-    },
-    optimizeDeps: {
-      include: [
-        `monaco-editor/esm/vs/language/json/json.worker`,
-        `monaco-editor/esm/vs/language/css/css.worker`,
-        `monaco-editor/esm/vs/language/html/html.worker`,
-        `monaco-editor/esm/vs/language/typescript/ts.worker`,
-        `monaco-editor/esm/vs/editor/editor.worker`
-      ], 
-    },
-    server: {
-      https: true,
-      proxy: {
-        '/api/v1/': {
-          target: '{{ .BK_BCS_BSCP_API }}',
-          changeOrigin: true,
-          secure: false
-        }
-      }
-    }
-  }
+	return {
+		base: './',
+		publicDir: 'static',
+		plugins,
+		resolve: {
+			alias: {
+				'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+			},
+		},
+		build: {
+			outDir: 'dist',
+			assetsDir: 'static',
+			target: 'es2015',
+			commonjsOptions: {
+				transformMixedEsModules: true,
+			},
+			rollupOptions: {
+				output: {
+					entryFileNames: 'static/js/[name]-[hash].js',
+					chunkFileNames: 'static/js/[name]-[hash].js',
+					assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+				},
+			},
+		},
+		optimizeDeps: {
+			include: [
+				`monaco-editor/esm/vs/language/json/json.worker`,
+				`monaco-editor/esm/vs/language/css/css.worker`,
+				`monaco-editor/esm/vs/language/html/html.worker`,
+				`monaco-editor/esm/vs/language/typescript/ts.worker`,
+				`monaco-editor/esm/vs/editor/editor.worker`,
+			],
+		},
+		server: {
+			https: false,
+			host: 'dev.bscp.site.bktencent.com',
+			port: 5174,
+			proxy: {
+				'/api/v1/': {
+					target: '{{ .BK_BCS_BSCP_API }}',
+					changeOrigin: true,
+					secure: false,
+				},
+			},
+		},
+	}
 })
