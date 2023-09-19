447972305bec8f069b2f4cc4fafbf0d004852973
Joe Lei <thezero12@hotmail.com>
2023-09-19 15:02:44 +0800

    Update: refine golangci style (#2591)
    * Update: refine golangci style
    
    * Update: fix License
    
    * Update: goheader add year

--- bcs-services/bcs-bscp/pkg/dal/sharding/sharding.go
+++ bcs-services/bcs-bscp/pkg/dal/sharding/sharding.go
@@ -1,14 +1,14 @@
 /*
-Tencent is pleased to support the open source community by making Basic Service Configuration Platform available.
-Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the "License"); you may not use this file except
-in compliance with the License. You may obtain a copy of the License at
-http://opensource.org/licenses/MIT
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "as IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-either express or implied. See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // Package sharding NOTES
 package sharding
@@ -17,9 +17,7 @@ import (
 	"context"
 	"fmt"
 
-	"bscp.io/pkg/cc"
-	"bscp.io/pkg/criteria/uuid"
-	"bscp.io/pkg/dal/gen"
+	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -28,7 +26,9 @@ import (
 	"gorm.io/sharding"
 	"k8s.io/klog/v2"
 
-	"github.com/jmoiron/sqlx"
+	"bscp.io/pkg/cc"
+	"bscp.io/pkg/criteria/uuid"
+	"bscp.io/pkg/dal/gen"
 )
 
 // InitSharding initialize a sharding management instance.
