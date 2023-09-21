4ea6b6443c142fe6a9fd8acb0cc0a5bf4b369b32
Yuikill <105910874+Yuikill@users.noreply.github.com>
2023-09-21 17:30:26 +0800

    fix: 修复查询模板搜索引用详情无效问题 (#2622)

--- bcs-services/bcs-bscp/ui/src/views/space/templates/version-manage/version-bound-by-apps-detail.vue
+++ bcs-services/bcs-bscp/ui/src/views/space/templates/version-manage/version-bound-by-apps-detail.vue
@@ -42,7 +42,7 @@
       limit: pagination.value.limit
     }
     if (searchStr.value) {
-      params.search_fields = 'name,id'
+      params.search_fields = 'app_name,template_revision_name'
       params.search_value = searchStr.value
     }
     const res = await getUnNamedVersionAppsBoundByTemplateVersion(props.spaceId, props.currentTemplateSpace, props.config.id, props.config.versionId, params)
