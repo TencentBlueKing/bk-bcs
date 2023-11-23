bc2725995423fa28755be42dda038c67908fda84
Luo Fan <44999219+luofann@users.noreply.github.com>
2023-11-23 14:46:59 +0800

    feat: 前端增加业务白名单逻辑 (#2771)

--- bcs-services/bcs-bscp/ui/src/views/space/index.vue
+++ bcs-services/bcs-bscp/ui/src/views/space/index.vue
@@ -1,22 +1,37 @@
 <template>
-  <apply-perm-page v-if="showPermApplyPage"></apply-perm-page>
-  <router-view v-else></router-view>
+  <template v-if="!loading">
+    <whitelist-apply-page v-if="!spaceFeatureFlags.BIZ_VIEW"></whitelist-apply-page>
+    <apply-perm-page v-else-if="showPermApplyPage"></apply-perm-page>
+    <router-view v-else></router-view>
+  </template>
 </template>
 <script setup lang="ts">
-import { watch } from 'vue';
+import { watch, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import useGlobalStore from '../../store/global';
+import { getSpaceFeatureFlag } from '../../api';
+import whitelistApplyPage from './whitelist-apply-page.vue';
 import applyPermPage from './apply-perm-page.vue';
 
-const { spaceId, showPermApplyPage } = storeToRefs(useGlobalStore());
+const { spaceId, spaceFeatureFlags, showPermApplyPage } = storeToRefs(useGlobalStore());
 
 const route = useRoute();
+
+const loading = ref(true);
+
+const getFeatureFlagsData = async() => {
+  loading.value = true;
+  const res = await getSpaceFeatureFlag(spaceId.value);
+  spaceFeatureFlags.value = res;
+  loading.value = false;
+}
 
 watch(
   () => route.params.spaceId,
   (val) => {
     spaceId.value = val as string;
+    getFeatureFlagsData();
   },
   { immediate: true },
 );
