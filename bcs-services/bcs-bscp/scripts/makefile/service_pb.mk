fd94b3b71a0e3f758c5ee84969cf48af48be9d74
joelei <thezero12@hotmail.com>
2023-12-09 23:37:11 +0800

    chore: refine protobuf

--- bcs-services/bcs-bscp/scripts/makefile/service_pb.mk
+++ bcs-services/bcs-bscp/scripts/makefile/service_pb.mk
@@ -10,7 +10,7 @@ GW:=$(patsubst %.proto, %.pb.gw.go, $(PROTO))
 
 all:
     ifeq ("$(VERSION)","libprotoc 22.0")
-		@protoc --proto_path=. --proto_path=../../../../ --proto_path=../../../pkg/thirdparty/protobuf/ --go_opt=paths=source_relative --go_out=. --go-grpc_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --grpc-gateway_opt=logtostderr=true --grpc-gateway_opt=paths=source_relative --grpc-gateway_out=. *.proto
+		@protoc --proto_path=. --proto_path=../../../ --proto_path=../../../pkg/thirdparty/protobuf/ --go_opt=paths=source_relative --go_out=. --go-grpc_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --grpc-gateway_opt=logtostderr=true --grpc-gateway_opt=paths=source_relative --grpc-gateway_out=. *.proto
     else
 		@echo "make pb failed, protoc version not 22.0"
 		exit 1
