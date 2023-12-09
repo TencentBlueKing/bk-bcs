422fea2ffbdd45f4e7e1bdfe9e3cb7994a0d2050
johnypeng <johnypeng@canway.net>
2023-12-09 18:15:45 +0800

    adjust README

--- bcs-services/bcs-bscp/pkg/i18n/translations/translations.go
+++ bcs-services/bcs-bscp/pkg/i18n/translations/translations.go
@@ -18,7 +18,7 @@ package translations
 * -srclang 指定在源码代中使用的语言，这里是英语en, 语言名称需符合BCP 47规范https://en.wikipedia.org/wiki/IETF_language_tag
 * update 该子命令用来从源码中提取要翻译的message以及进行合并操作，并生成对应的目录文件
 * -out 指定要生成的message catalog文件，主要是存放不同语言翻译前后的映射关系
-* -lang 指定要翻译的目标语言，这里是英语en和中文zh，这里将源码使用语言en也带上便于集中查看比较，多个语言之间以逗号分隔
+* -lang 指定要翻译的目标语言，这里是英语en和中文zh，多个语言之间以逗号分隔
 * 最后的参数表示要提取翻译message的包路径，多个包路径以空格分隔
 *
 * gotext命令行工具更多使用，见gotext help输出
