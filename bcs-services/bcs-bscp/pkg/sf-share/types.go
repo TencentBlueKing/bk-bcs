f78c8e33e256e67e96ef654557b3d49f1c4a6726
ifooth <thezero12@hotmail.com>
2023-12-11 21:42:54 +0800

    feat: GetKvValueReq add match and validate --story=115482064

--- bcs-services/bcs-bscp/pkg/sf-share/types.go
+++ bcs-services/bcs-bscp/pkg/sf-share/types.go
@@ -200,7 +200,7 @@ type InstanceSpec struct {
 	App        string            `json:"app"`
 	Uid        string            `json:"uid"`
 	Labels     map[string]string `json:"labels"`
-	ConfigType table.ConfigType  `json:"config_type"`
+	ConfigType table.ConfigType  `json:"configType"`
 }
 
 // Validate the instance spec is valid or not
