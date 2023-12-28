cc3713a7fc2d97c4632420cadb0b5b4cc38a2694
LidolLxf <13660354959@163.com>
2023-12-28 16:27:17 +0800

    规范处理

--- bcs-services/bcs-cluster-manager/internal/utils/audit.go
+++ bcs-services/bcs-cluster-manager/internal/utils/audit.go
@@ -4,7 +4,7 @@
  * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under,
+ * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,13 +19,13 @@ import (
 	"reflect"
 	"time"
 
+	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
+	"github.com/Tencent/bk-bcs/bcs-common/pkg/audit"
+	"github.com/Tencent/bk-bcs/bcs-common/pkg/odm/drivers"
 	"github.com/micro/go-micro/v2/metadata"
 	"github.com/micro/go-micro/v2/server"
 	grpcmeta "google.golang.org/grpc/metadata"
 
-	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
-	"github.com/Tencent/bk-bcs/bcs-common/pkg/audit"
-	"github.com/Tencent/bk-bcs/bcs-common/pkg/odm/drivers"
 	"github.com/Tencent/bk-bcs/bcs-services/bcs-cluster-manager/internal/auth"
 	"github.com/Tencent/bk-bcs/bcs-services/bcs-cluster-manager/internal/common"
 	raudit "github.com/Tencent/bk-bcs/bcs-services/bcs-cluster-manager/internal/remote/audit"
@@ -97,6 +97,7 @@ func getResourceID(req server.Request) resource {
 	return resourceID
 }
 
+// nolint
 var auditFuncMap = map[string]func(req server.Request, rsp interface{}) (audit.Resource, audit.Action){
 	"ClusterManager.CreateCluster": func(req server.Request, rsp interface{}) (audit.Resource, audit.Action) {
 		res := getResourceID(req)
@@ -324,7 +325,7 @@ func addAudit(ctx context.Context, req server.Request, rsp interface{}, startTim
 	if result.ResultCode != types.BcsErrClusterManagerSuccess {
 		result.Status = audit.ActivityStatusFailed
 	}
-	raudit.GetAuditClient().R().
+	_ = raudit.GetAuditClient().R().
 		SetContext(auditCtx).SetResource(resource).SetAction(action).SetResult(result).Do()
 }
 
