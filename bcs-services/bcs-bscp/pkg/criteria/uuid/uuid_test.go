031b2397e49b092b8b26334711acfc35a4140e11
ifooth <thezero12@hotmail.com>
2024-01-11 11:42:34 +0800

    Feat: refine sf metadata

--- bcs-services/bcs-bscp/pkg/criteria/uuid/uuid_test.go
+++ bcs-services/bcs-bscp/pkg/criteria/uuid/uuid_test.go
@@ -14,16 +14,17 @@ package uuid
 
 import "testing"
 
+// BenchmarkUUID
+// Benchmark Result
+// 1: pborman/uuid
+// 2: github.com/google/uuid
+// 3: github.com/google/uuid with uuid.EnableRandPool()
+
+// BenchmarkUUID-16         5305968               223.3 ns/op            64 B/op          2 allocs/op
+// BenchmarkUUID-16          815874               1463 ns/op             64 B/op          2 allocs/op
+// BenchmarkUUID-16         3996272               279.6 ns/op            48 B/op          1 allocs/op
 func BenchmarkUUID(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = UUID()
 	}
-	// Benchmark Result
-	// 1: pborman/uuid
-	// 2: github.com/google/uuid
-	// 3: github.com/google/uuid with uuid.EnableRandPool()
-
-	// BenchmarkUUID-16         5305968               223.3 ns/op            64 B/op          2 allocs/op
-	// BenchmarkUUID-16          815874               1463 ns/op             64 B/op          2 allocs/op
-	// BenchmarkUUID-16         3996272               279.6 ns/op            48 B/op          1 allocs/op
 }
