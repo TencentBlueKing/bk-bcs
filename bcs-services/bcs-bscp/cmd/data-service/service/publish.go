403f218c268d75318862c35d48751f0fb7a9076e
AlkaidChen <362774405@qq.com>
2024-01-17 14:22:57 +0800

    feat(bscp): check if app have bound credentials when being published (#2904)
    * feat(bscp): check if app have bound credentials when being published
    
    * feat(bscp): check if app have bound credentials when being published
    
    * feat(bscp): check if app have bound credentials when being published
    
    * feat(bscp): check if app have bound credentials when being published

--- bcs-services/bcs-bscp/cmd/data-service/service/publish.go
+++ bcs-services/bcs-bscp/cmd/data-service/service/publish.go
@@ -33,6 +33,7 @@ import (
 )
 
 // Publish exec publish strategy.
+// nolint: funlen
 func (s *Service) Publish(ctx context.Context, req *pbds.PublishReq) (*pbds.PublishResp, error) {
 	grpcKit := kit.FromGrpcContext(ctx)
 
@@ -110,13 +111,60 @@ func (s *Service) Publish(ctx context.Context, req *pbds.PublishReq) (*pbds.Publ
 		}
 		return nil, err
 	}
+	haveCredentials, err := s.checkAppHaveCredentials(grpcKit, req.BizId, req.AppId)
+	if err != nil {
+		if rErr := tx.Rollback(); rErr != nil {
+			logs.Errorf("transaction rollback failed, err: %v, rid: %s", rErr, grpcKit.Rid)
+		}
+		return nil, err
+	}
 	if err := tx.Commit(); err != nil {
 		logs.Errorf("commit transaction failed, err: %v, rid: %s", err, grpcKit.Rid)
 		return nil, err
 	}
 
-	resp := &pbds.PublishResp{PublishedStrategyHistoryId: pshID}
+	resp := &pbds.PublishResp{
+		PublishedStrategyHistoryId: pshID,
+		HaveCredentials:            haveCredentials,
+	}
 	return resp, nil
+}
+
+// checkAppHaveCredentials check if there is available credential for app.
+// 1. credential scope can match app name.
+// 2. credential is enabled.
+func (s *Service) checkAppHaveCredentials(grpcKit *kit.Kit, bizID, appID uint32) (bool, error) {
+	app, err := s.dao.App().Get(grpcKit, bizID, appID)
+	if err != nil {
+		return false, err
+	}
+	matchedCredentials := make([]uint32, 0)
+	scopes, err := s.dao.CredentialScope().ListAll(grpcKit, bizID)
+	if err != nil {
+		return false, err
+	}
+	if len(scopes) == 0 {
+		return false, nil
+	}
+	for _, scope := range scopes {
+		match, e := scope.Spec.CredentialScope.MatchApp(app.Spec.Name)
+		if e != nil {
+			return false, e
+		}
+		if match {
+			matchedCredentials = append(matchedCredentials, scope.Attachment.CredentialId)
+		}
+	}
+	credentials, e := s.dao.Credential().BatchListByIDs(grpcKit, bizID, matchedCredentials)
+	if e != nil {
+		return false, e
+	}
+	for _, credential := range credentials {
+		if credential.Spec.Enable {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 // GenerateReleaseAndPublish generate release and publish.
