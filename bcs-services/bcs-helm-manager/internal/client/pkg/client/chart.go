d087b6ad575a340921ac2e0d16f37eae2e055597
LidolLxf <130628034+LidolLxf@users.noreply.github.com>
2024-01-30 14:44:43 +0800

    feat：helmctl 添加上传 chart 的命令 (#2906)
    * feat：helmctl 添加上传 chat 的命令
    
    * 规范修改
    
    * feat：helmctl 添加上传 chart 的命令优化

--- bcs-services/bcs-helm-manager/internal/client/pkg/client/chart.go
+++ bcs-services/bcs-helm-manager/internal/client/pkg/client/chart.go
@@ -13,13 +13,25 @@
 package client
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strconv"
 
+	"helm.sh/helm/v3/pkg/chartutil"
+
 	"github.com/Tencent/bk-bcs/bcs-services/bcs-helm-manager/internal/client/pkg"
+	utilschart "github.com/Tencent/bk-bcs/bcs-services/bcs-helm-manager/internal/utils/chart"
+	"github.com/Tencent/bk-bcs/bcs-services/bcs-helm-manager/internal/utils/httpx"
 	helmmanager "github.com/Tencent/bk-bcs/bcs-services/bcs-helm-manager/proto/bcs-helm-manager"
 )
 
@@ -30,6 +42,7 @@ const (
 	urlChartVersionList   = "/projects/%s/repos/%s/charts/%s/versions"
 	urlChartVersionDetail = "/projects/%s/repos/%s/charts/%s/versions/%s"
 	urlChartVersionDelete = "/projects/%s/repos/%s/charts/%s/versions/%s"
+	urlChartCreate        = "/projects/%s/repos/%s/charts/upload"
 )
 
 // Chart return a pkg.ChartClient instance
@@ -250,6 +263,198 @@ func (ct *chart) DeleteChartVersion(ctx context.Context, req *helmmanager.Delete
 	}
 
 	return nil
+}
+
+// PushChart push chart
+func (ct *chart) PushChart(ctx context.Context, req *utilschart.PushChart) error {
+	if req == nil {
+		return fmt.Errorf("push chart request is empty")
+	}
+
+	if req.RepoName == "" {
+		return fmt.Errorf("repository name can not be empty")
+	}
+
+	if req.ProjectCode == "" {
+		return fmt.Errorf("repository projectCode can not be empty")
+	}
+
+	fi, err := os.Stat(req.FilePath)
+	if err != nil {
+		return err
+	}
+
+	// 文件夹需要压缩，生成的tgz文件需要删除
+	if fi.IsDir() {
+		req.FilePath, err = getTgz(req.FilePath)
+		if err != nil {
+			return err
+		}
+
+		defer func(path string) {
+			err = os.Remove(req.FilePath)
+			if err != nil {
+				fmt.Printf("Failed to remove tgz file %s: %s", path, err)
+			}
+		}(req.FilePath)
+	}
+
+	data, header, err := getFileData(req.FilePath)
+	if err != nil {
+		return err
+	}
+
+	resp, err := ct.post(ctx, urlUploadChartPrefix+fmt.Sprintf(urlChartCreate, req.ProjectCode, req.RepoName)+
+		ct.createChartQuery(req.Version, req.Force), header, data)
+	if err != nil {
+		return err
+	}
+
+	var r httpx.BaseResponse
+	if err = json.Unmarshal(resp.Reply, &r); err != nil {
+		return err
+	}
+
+	if r.Code != resultCodeSuccess {
+		return fmt.Errorf("push chart get result code %d, message: %s", r.Code, r.Message)
+	}
+
+	return nil
+}
+
+// 将文件夹进行压缩
+func getTgz(filePath string) (string, error) {
+	// 转成所在系统合法路径
+	name := filepath.FromSlash(filePath)
+
+	if validChart, err := chartutil.IsChartDir(name); !validChart {
+		return "", err
+	}
+	// 获取绝对路径
+	chartPath, err := filepath.Abs(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	// 获取文件夹路径及名称
+	dir, name := filepath.Split(chartPath)
+	tgzPath := filepath.Join(dir, name+".tgz")
+	file, err := os.Create(tgzPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to create destination file: %w", err)
+	}
+	defer file.Close() // nolint
+
+	// 创建 gzip.Writer
+	gzipWriter := gzip.NewWriter(file)
+	defer gzipWriter.Close() // nolint
+
+	// 创建 tar.Writer
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer tarWriter.Close() // nolint
+
+	// 遍历源文件夹中的所有文件和子文件夹
+	err = filepath.Walk(chartPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("error walking through directory: %w", err)
+		}
+		// 压缩包里面的放置需要以路径分离后chartPath的文件夹名称为初始文件夹
+		_, name := filepath.Split(chartPath)
+		// 获取相对路径
+		relativePath, err := filepath.Rel(chartPath, path)
+		if err != nil {
+			return fmt.Errorf("failed to get relative path: %w", err)
+		}
+
+		relativePath = filepath.Join(name, relativePath)
+
+		// 创建 tar.Header
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return fmt.Errorf("failed to create tar header: %w", err)
+		}
+
+		// 设置相对路径
+		header.Name = relativePath
+
+		// 写入 header
+		err = tarWriter.WriteHeader(header)
+		if err != nil {
+			return fmt.Errorf("failed to write tar header: %w", err)
+		}
+
+		// 如果是文件，将文件内容写入 tar.Writer
+		if !info.IsDir() {
+			file, err := os.Open(path)
+			if err != nil {
+				return fmt.Errorf("failed to open file: %w", err)
+			}
+			defer file.Close()
+
+			_, err = io.Copy(tarWriter, file)
+			if err != nil {
+				return fmt.Errorf("failed to write file content to tar writer: %w", err)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("error compressing to TGZ: %w", err)
+	}
+
+	return tgzPath, nil
+}
+
+// 生成文件流及返回Header
+func getFileData(filePath string) ([]byte, http.Header, error) {
+	if filePath == "" {
+		return nil, nil, fmt.Errorf("empty param data")
+	}
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+
+	// 创建一个新的multipart请求体
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+
+	header := http.Header{}
+	header.Add("Content-Type", writer.FormDataContentType())
+	part, err := writer.CreateFormFile("chart", filePath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	_, err = io.Copy(part, f)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// 完成multipart请求体的创建
+	err = writer.Close()
+	if err != nil {
+		return nil, nil, err
+	}
+	return body.Bytes(), header, nil
+}
+
+func (ct *chart) createChartQuery(version string, force bool) string {
+	query := url.Values{}
+	if version != "" {
+		query.Set("version", version)
+	}
+	if force {
+		query.Set("force", "true")
+	}
+	if len(query) == 0 {
+		return ""
+	}
+	return "?" + query.Encode()
 }
 
 // List chart
