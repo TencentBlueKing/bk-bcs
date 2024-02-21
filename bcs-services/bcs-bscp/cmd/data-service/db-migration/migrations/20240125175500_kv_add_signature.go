708d23aa56a6eb0bdf6c0019586e3fc678921cc0
ifooth <thezero12@hotmail.com>
2024-02-21 15:54:50 +0800

    feat: kv支持返回 sha256 签名 --task=74821337

--- bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/20240125175500_kv_add_signature.go
+++ bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/20240125175500_kv_add_signature.go
@@ -13,8 +13,6 @@
 package migrations
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 
 	"github.com/TencentBlueKing/bk-bcs/bcs-services/bcs-bscp/cmd/data-service/db-migration/migrator"
@@ -31,103 +29,65 @@ func init() {
 	})
 }
 
+// Kvs20240125175500 kv
+type Kvs20240125175500 struct {
+	Signature string `gorm:"type:varchar(64) not null"`
+	ByteSize  uint   `gorm:"type:bigint(1) unsigned not null"`
+}
+
+// TableName gorm table name
+func (Kvs20240125175500) TableName() string {
+	return "kvs"
+}
+
+// ReleasedKvs20240125175500 已生成版本的kv
+type ReleasedKvs20240125175500 struct {
+	Signature string `gorm:"type:varchar(64) not null"`
+	ByteSize  uint   `gorm:"type:bigint(1) unsigned not null"`
+}
+
+// TableName gorm table name
+func (ReleasedKvs20240125175500) TableName() string {
+	return "released_kvs"
+}
+
 // mig20240125175500Up for up migration
 func mig20240125175500Up(tx *gorm.DB) error {
-	// Kvs : kv
-	type Kvs struct {
-		ID uint `gorm:"type:bigint(1) unsigned not null;primaryKey;autoIncrement:false"`
-
-		// Spec is specifics of the resource defined with user
-		Key     string `gorm:"type:varchar(255) not null;uniqueIndex:idx_bizID_appID_key_kvState,priority:1"`
-		Version uint   `gorm:"type:bigint(1) unsigned not null;"`
-		KvType  string `gorm:"type:varchar(64) not null"`
-		KvState string `gorm:"type:varchar(64) not null;uniqueIndex:idx_bizID_appID_key_kvState,priority:2"`
-
-		// Attachment is attachment info of the resource
-		BizID uint `gorm:"type:bigint(1) unsigned not null;uniqueIndex:idx_bizID_appID_key_kvState,priority:3"`
-		APPID uint `gorm:"type:bigint(1) unsigned not null;uniqueIndex:idx_bizID_appID_key_kvState,priority:4"`
-
-		Signature string `gorm:"type:varchar(64) not null"`
-		ByteSize  uint   `gorm:"type:bigint(1) unsigned not null"`
-
-		// Revision is revision info of the resource
-		Creator   string    `gorm:"type:varchar(64) not null"`
-		Reviser   string    `gorm:"type:varchar(64) not null"`
-		CreatedAt time.Time `gorm:"type:datetime(6) not null"`
-		UpdatedAt time.Time `gorm:"type:datetime(6) not null"`
-	}
-
-	// ReleasedKvs :已生成版本的kv
-	type ReleasedKvs struct {
-		ID uint `gorm:"type:bigint(1) unsigned not null;primaryKey;autoIncrement:false"`
-
-		// Spec is specifics of the resource defined with user
-		Key       string `gorm:"type:varchar(255) not null;uniqueIndex:relID_key,priority:1"`
-		Version   uint   `gorm:"type:bigint(1) unsigned not null;"`
-		ReleaseID uint   `gorm:"type:bigint(1) unsigned not null;index:idx_bizID_appID_ID,priority:3;uniqueIndex:relID_key,priority:2"` //nolint:lll
-		KvType    string `gorm:"type:varchar(64) not null"`
-
-		// Attachment is attachment info of the resource
-		BizID uint `gorm:"type:bigint(1) unsigned not null;index:idx_bizID_appID_ID,priority:1"`
-		AppID uint `gorm:"type:bigint(1) unsigned not null;index:idx_bizID_appID_ID,priority:2"`
-
-		Signature string `gorm:"type:varchar(64) not null"`
-		ByteSize  uint   `gorm:"type:bigint(1) unsigned not null"`
-
-		// Revision is revision info of the resource
-		Creator   string    `gorm:"type:varchar(64) not null"`
-		Reviser   string    `gorm:"type:varchar(64) not null"`
-		CreatedAt time.Time `gorm:"type:datetime(6) not null"`
-		UpdatedAt time.Time `gorm:"type:datetime(6) not null"`
-	}
-
 	if err := tx.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4").
-		AutoMigrate(&Kvs{}); err != nil {
+		AutoMigrate(&Kvs20240125175500{}); err != nil {
 		return err
 	}
 
 	if err := tx.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4").
-		AutoMigrate(&ReleasedKvs{}); err != nil {
+		AutoMigrate(&ReleasedKvs20240125175500{}); err != nil {
 		return err
 	}
 
 	return nil
-
 }
 
 // mig20240125175500Down for down migration
 func mig20240125175500Down(tx *gorm.DB) error {
-	// Kvs: 服务
-	type Kvs struct {
-		Signature string `gorm:"type:varchar(64) not null"`
-		ByteSize  uint   `gorm:"type:bigint(1) unsigned not null"`
-	}
-
-	// ReleasedKvs: 服务
-	type ReleasedKvs struct {
-		Signature string `gorm:"type:varchar(64) not null"`
-		ByteSize  uint   `gorm:"type:bigint(1) unsigned not null"`
-	}
-
-	// delete old column
-	if tx.Migrator().HasColumn(&Kvs{}, "signature") {
-		if err := tx.Migrator().DropColumn(&Kvs{}, "signature"); err != nil {
+	// delete kvs old column
+	if tx.Migrator().HasColumn(&Kvs20240125175500{}, "Signature") {
+		if err := tx.Migrator().DropColumn(&Kvs20240125175500{}, "Signature"); err != nil {
 			return err
 		}
 	}
-	if tx.Migrator().HasColumn(&Kvs{}, "byte_size") {
-		if err := tx.Migrator().DropColumn(&Kvs{}, "byte_size"); err != nil {
+	if tx.Migrator().HasColumn(&Kvs20240125175500{}, "ByteSize") {
+		if err := tx.Migrator().DropColumn(&Kvs20240125175500{}, "ByteSize"); err != nil {
 			return err
 		}
 	}
 
-	if tx.Migrator().HasColumn(&ReleasedKvs{}, "signature") {
-		if err := tx.Migrator().DropColumn(&ReleasedKvs{}, "signature"); err != nil {
+	// delete release_kvs old column
+	if tx.Migrator().HasColumn(&ReleasedKvs20240125175500{}, "Signature") {
+		if err := tx.Migrator().DropColumn(&ReleasedKvs20240125175500{}, "Signature"); err != nil {
 			return err
 		}
 	}
-	if tx.Migrator().HasColumn(&ReleasedKvs{}, "byte_size") {
-		if err := tx.Migrator().DropColumn(&ReleasedKvs{}, "byte_size"); err != nil {
+	if tx.Migrator().HasColumn(&ReleasedKvs20240125175500{}, "ByteSize") {
+		if err := tx.Migrator().DropColumn(&ReleasedKvs20240125175500{}, "ByteSize"); err != nil {
 			return err
 		}
 	}
