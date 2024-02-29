052bccda2616438ad9b5a87c540070f4f5bbd216
LidolLxf <13660354959@163.com>
2024-02-29 16:32:43 +0800

    /bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver规范修改

--- bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver/pkg/rest/request.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver/pkg/rest/request.go
@@ -4,7 +4,7 @@
  * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under,
+ * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,12 +18,13 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver/pkg/clientutil"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
 	apirequest "k8s.io/apiserver/pkg/endpoints/request"
+
+	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-unified-apiserver/pkg/clientutil"
 )
 
 const (
