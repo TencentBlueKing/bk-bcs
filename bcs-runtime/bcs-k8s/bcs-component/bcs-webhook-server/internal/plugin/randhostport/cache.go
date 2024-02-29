3291ea31fb7444412410f217e523c48264f3141e
LidolLxf <13660354959@163.com>
2024-02-29 18:11:25 +0800

    bcs-netservice-controller、bcs-webhook-server规范修改

--- bcs-runtime/bcs-k8s/bcs-component/bcs-webhook-server/internal/plugin/randhostport/cache.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-webhook-server/internal/plugin/randhostport/cache.go
@@ -4,7 +4,7 @@
  * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under,
+ * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -121,7 +121,7 @@ func (pc *PortCache) Unlock() {
 // IncPortQuantity increase quantity for certain port
 func (pc *PortCache) IncPortQuantity(port uint64) error {
 	if entry, ok := pc.m[port]; ok {
-		entry.Quantity = entry.Quantity + 1
+		entry.Quantity++
 		heap.Fix(pc.h, entry.Index)
 	}
 	return fmt.Errorf("entry for port %d not found when do increase", port)
@@ -130,7 +130,7 @@ func (pc *PortCache) IncPortQuantity(port uint64) error {
 // DecPortQuantity decrease quantity for certain port
 func (pc *PortCache) DecPortQuantity(port uint64) error {
 	if entry, ok := pc.m[port]; ok {
-		entry.Quantity = entry.Quantity - 1
+		entry.Quantity--
 		heap.Fix(pc.h, entry.Index)
 		return nil
 	}
--- bcs-runtime/bcs-k8s/bcs-component/bcs-webhook-server/internal/server/mesos.go
+++ bcs-runtime/bcs-k8s/bcs-component/bcs-webhook-server/internal/server/mesos.go
@@ -1,10 +1,10 @@
 /*
- * Tencent is pleased to support the open source community by making Blueking Container Service available.,
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
  * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
  * Licensed under the MIT License (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under,
+ * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -22,6 +22,7 @@ import (
 
 	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
 	commtypes "github.com/Tencent/bk-bcs/bcs-common/common/types"
+
 	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-webhook-server/internal/metrics"
 	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-component/bcs-webhook-server/internal/types"
 )
@@ -33,6 +34,7 @@ type Meta struct {
 }
 
 // MesosHook do mesos hook
+// nolint funlen
 func (ws *WebhookServer) MesosHook(w http.ResponseWriter, r *http.Request) {
 	var (
 		handler = "MesosHook"
