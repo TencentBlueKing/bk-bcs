8ad2c497f8fd74a6dc417da0c4462a85f1b6ba6f
johnypeng <johnypeng@canway.net>
2024-03-02 17:52:45 +0800

    chore: adjust collation to utf8mb4_bin

--- bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/20240228173324_modify_name_column.go
+++ bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/20240228173324_modify_name_column.go
@@ -30,14 +30,14 @@ func init() {
 }
 
 // mig20240228173324Up for up migration
-// 字段变更，设置为collate为utf8_bin，查询时区分大小写，场景如：字段唯一性校验时，字符串"App"和"app"能区分为不同
+// 字段变更，设置为collate为utf8mb4_bin，查询时区分大小写，场景如：字段唯一性校验时，字符串"App"和"app"能区分为不同
 //
 //nolint:funlen
 func mig20240228173324Up(tx *gorm.DB) error {
 	// Applications : 服务
 	type Applications struct {
-		Name  string `gorm:"type:varchar(255) collate utf8_bin not null"`
-		Alias string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name  string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
+		Alias string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Applications{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&Applications{}, "Name"); err != nil {
@@ -52,8 +52,8 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// ConfigItems : 配置项
 	type ConfigItems struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
-		Path string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
+		Path string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&ConfigItems{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&ConfigItems{}, "Name"); err != nil {
@@ -68,7 +68,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// Credentials : 密钥
 	type Credentials struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Credentials{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&Credentials{}, "Name"); err != nil {
@@ -78,7 +78,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// Groups : 分组
 	type Groups struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Groups{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&Groups{}, "Name"); err != nil {
@@ -88,7 +88,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// HookRevisions : 脚本版本
 	type HookRevisions struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&HookRevisions{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&HookRevisions{}, "Name"); err != nil {
@@ -98,7 +98,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// Hooks : 脚本
 	type Hooks struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Hooks{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&Hooks{}, "Name"); err != nil {
@@ -108,7 +108,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// Kvs : 键值对配置
 	type Kvs struct {
-		Key string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Key string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Kvs{}, "Key") {
 		if err := tx.Migrator().AlterColumn(&Kvs{}, "Key"); err != nil {
@@ -118,7 +118,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// Releases : 版本
 	type Releases struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Releases{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&Releases{}, "Name"); err != nil {
@@ -128,7 +128,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// TemplateRevisions : 模版版本
 	type TemplateRevisions struct {
-		RevisionName string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		RevisionName string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&TemplateRevisions{}, "RevisionName") {
 		if err := tx.Migrator().AlterColumn(&TemplateRevisions{}, "RevisionName"); err != nil {
@@ -138,7 +138,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// TemplateSets : 模版套餐
 	type TemplateSets struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&TemplateSets{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&TemplateSets{}, "Name"); err != nil {
@@ -148,7 +148,7 @@ func mig20240228173324Up(tx *gorm.DB) error {
 
 	// TemplateSpaces : 模版空间
 	type TemplateSpaces struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&TemplateSpaces{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&TemplateSpaces{}, "Name"); err != nil {
@@ -156,12 +156,28 @@ func mig20240228173324Up(tx *gorm.DB) error {
 		}
 	}
 
+	// TemplateVariables : 模版变量
+	type TemplateVariables struct {
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
+	}
+	if tx.Migrator().HasColumn(&TemplateVariables{}, "Name") {
+		if err := tx.Migrator().AlterColumn(&TemplateVariables{}, "Name"); err != nil {
+			return err
+		}
+	}
+
 	// Templates : 模版
 	type Templates struct {
-		Name string `gorm:"type:varchar(255) collate utf8_bin not null"`
+		Name string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
+		Path string `gorm:"type:varchar(255) collate utf8mb4_bin not null"`
 	}
 	if tx.Migrator().HasColumn(&Templates{}, "Name") {
 		if err := tx.Migrator().AlterColumn(&Templates{}, "Name"); err != nil {
+			return err
+		}
+	}
+	if tx.Migrator().HasColumn(&Templates{}, "Path") {
+		if err := tx.Migrator().AlterColumn(&Templates{}, "Path"); err != nil {
 			return err
 		}
 	}
