f7053e60da12a021eab6de0ae7f61099d88ff966
AlkaidChan <362774405@qq.com>
2024-03-11 16:40:04 +0800

    fix(migrations): fix migrations

--- bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/20240307105336_add_client.go
+++ bcs-services/bcs-bscp/cmd/data-service/db-migration/migrations/20240307105336_add_client.go
@@ -60,6 +60,31 @@ func mig20240307105336Up(tx *gorm.DB) error {
 		FailedDetailReason        string    `gorm:"column:failed_detail_reason;type:varchar(255);default:'';NOT NULL"`
 	}
 
+	// ClientEvents : ClientEvents
+	type ClientEvents struct {
+		ID       uint   `gorm:"column:id;type:bigint(1) unsigned;primary_key"`
+		ClientID uint   `gorm:"column:client_id;type:bigint(1) unsigned;NOT NULL;index:idx_clientID,priority:1"`
+		CursorID string `gorm:"column:cursor_id;type:varchar(128);NOT NULL;uniqueIndex:idx_bizID_appID_uid_cursorID,priority:1"` // nolint
+
+		BizID uint `gorm:"type:bigint(1) unsigned not null;uniqueIndex:idx_bizID_appID_uid_cursorID,priority:2"`
+		AppID uint `gorm:"type:bigint(1) unsigned not null;uniqueIndex:idx_bizID_appID_uid_cursorID,priority:3"`
+
+		UID                       string    `gorm:"column:uid;type:varchar(64);NOT NULL;uniqueIndex:idx_bizID_appID_uid_cursorID,priority:4"` // nolint
+		ClientMode                string    `gorm:"column:client_mode;type:varchar(20);default:'';NOT NULL;"`
+		OriginalReleaseID         uint      `gorm:"column:original_release_id;type:bigint(1) unsigned;default:0;NOT NULL"`
+		TargetReleaseID           uint      `gorm:"column:target_release_id;type:bigint(1) unsigned;default:0;NOT NULL"`
+		StartTime                 time.Time `gorm:"column:start_time;type:datetime(6);default:NULL"`
+		EndTime                   time.Time `gorm:"column:end_time;type:datetime(6);default:NULL"`
+		TotalSeconds              float64   `gorm:"column:total_seconds;type:double unsigned;default:0;NOT NULL"`
+		TotalFileSize             float64   `gorm:"column:total_file_size;type:double unsigned;default:0;NOT NULL"`
+		DownloadFileSize          float64   `gorm:"column:download_file_size;type:double unsigned;default:0;NOT NULL"`
+		TotalFileNum              uint      `gorm:"column:total_file_num;type:int(3) unsigned;default:0;NOT NULL"`
+		DownloadFileNum           uint      `gorm:"column:download_file_num;type:int(3) unsigned;default:0;NOT NULL"`
+		ReleaseChangeStatus       string    `gorm:"column:release_change_status;type:varchar(20);default:;NOT NULL"`
+		ReleaseChangeFailedReason string    `gorm:"column:release_change_failed_reason;type:varchar(20);default:'';NOT NULL"`
+		FailedDetailReason        string    `gorm:"column:failed_detail_reason;type:varchar(255);default:'';NOT NULL"`
+	}
+
 	// IDGenerators : ID生成器
 	type IDGenerators struct {
 		ID        uint      `gorm:"type:bigint(1) unsigned not null;primaryKey"`
@@ -73,9 +98,21 @@ func mig20240307105336Up(tx *gorm.DB) error {
 		return err
 	}
 
+	if err := tx.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4").
+		AutoMigrate(&ClientEvents{}); err != nil {
+		return err
+	}
+
 	now := time.Now()
+
 	if result := tx.Create([]IDGenerators{
 		{Resource: "clients", MaxID: 0, UpdatedAt: now},
+	}); result.Error != nil {
+		return result.Error
+	}
+
+	if result := tx.Create([]IDGenerators{
+		{Resource: "client_events", MaxID: 0, UpdatedAt: now},
 	}); result.Error != nil {
 		return result.Error
 	}
@@ -85,8 +122,26 @@ func mig20240307105336Up(tx *gorm.DB) error {
 
 // mig20240307105336Down for down migration
 func mig20240307105336Down(tx *gorm.DB) error {
-	if err := tx.Migrator().DropTable("model_example"); err != nil {
+
+	// IDGenerators : ID生成器
+	type IDGenerators struct {
+		ID        uint      `gorm:"type:bigint(1) unsigned not null;primaryKey"`
+		Resource  string    `gorm:"type:varchar(50) not null;uniqueIndex:idx_resource"`
+		MaxID     uint      `gorm:"type:bigint(1) unsigned not null"`
+		UpdatedAt time.Time `gorm:"type:datetime(6) not null"`
+	}
+
+	if err := tx.Migrator().
+		DropTable("clients", "client_events"); err != nil {
 		return err
+	}
+
+	var resources = []string{
+		"clients",
+		"client_events",
+	}
+	if result := tx.Where("resource IN ?", resources).Delete(&IDGenerators{}); result.Error != nil {
+		return result.Error
 	}
 
 	return nil
