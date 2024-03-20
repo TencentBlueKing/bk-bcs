be406cac9a2960ea859b705f70e9404da6be3731
AlkaidChen <362774405@qq.com>
2024-03-20 14:58:24 +0800

    feat(bcs-bscp): register system to bknotice at start up (#3056)
    * feat(bcs-bscp): register system to bknotice at start up
    
    * feat(bcs-bscp): register system to bknotice at start up

--- bcs-services/bcs-bscp/pkg/components/bknotice/bknotice.go
+++ bcs-services/bcs-bscp/pkg/components/bknotice/bknotice.go
@@ -28,6 +28,30 @@ type registerSystemResp struct {
 	Message string `json:"message"`
 }
 
+type getAnnouncementResp struct {
+	Result  bool           `json:"result"`
+	Code    int            `json:"code"`
+	Message string         `json:"message"`
+	Data    []Announcement `json:"data"`
+}
+
+// Announcement 通知中心公告
+type Announcement struct {
+	ID           int                   `json:"id"`
+	Title        string                `json:"title"`
+	Content      string                `json:"content"`
+	ContentList  []AnnouncementContent `json:"content_list"`
+	AnnounceType string                `json:"announce_type"`
+	StartTime    string                `json:"start_time"`
+	EndTime      string                `json:"end_time"`
+}
+
+// AnnouncementContent 通知中心公告内容，包括语言和内容
+type AnnouncementContent struct {
+	Content  string `json:"content"`
+	Language string `json:"language"`
+}
+
 // RegisterSystem 注册系统到通知中心
 func RegisterSystem(ctx context.Context) error {
 	url := fmt.Sprintf("%s/v1/register/", cc.ApiServer().BKNotice.Host)
@@ -54,4 +78,30 @@ func RegisterSystem(ctx context.Context) error {
 			resigerResp.Code, resigerResp.Message)
 	}
 	return nil
+}
+
+// GetCurrentAnnouncements 获取系统当前通知
+func GetCurrentAnnouncements(ctx context.Context, lang string) ([]Announcement, error) {
+
+	url := fmt.Sprintf("%s/v1/announcement/get_current_announcements/?platform=%s&language=%s",
+		cc.ApiServer().BKNotice.Host, cc.ApiServer().Esb.AppCode, lang)
+
+	authHeader := fmt.Sprintf("{\"bk_app_code\": \"%s\", \"bk_app_secret\": \"%s\"}",
+		cc.ApiServer().Esb.AppCode, cc.ApiServer().Esb.AppSecret)
+
+	resp, err := components.GetClient().R().
+		SetContext(ctx).
+		SetHeader("X-Bkapi-Authorization", authHeader).
+		Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	getAnnouncementResp := &getAnnouncementResp{}
+
+	if err := json.Unmarshal(resp.Body(), getAnnouncementResp); err != nil {
+		return nil, err
+	}
+	return getAnnouncementResp.Data, nil
 }
