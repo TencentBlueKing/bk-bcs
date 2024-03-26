56878b273ef871f59d00d227cd121678f11c71f3
Yuikill <105910874+Yuikill@users.noreply.github.com>
2024-03-26 19:15:10 +0800

    feat: 客户端查询功能 (#3048)
    * feat: 完成客户端管理路由搭建和头部交互
    
    * feat: 完成一级路由搭建
    
    * feat: 完成客户端搜索框的交互
    
    * feat: 完成客户端表格交互
    
    * feat: 客户端查询表格添加筛选功能
    
    * fix: 隐藏客户端统计入口
    
    * fix: 调整请求接口地址
    
    * fix: 调整拉取文件数和文件大小的展示效果

---
+++ bcs-services/bcs-bscp/ui/src/api/client.ts
@@ -0,0 +1,64 @@
+import http from '../request';
+import { IClinetCommonQuery, ICreateClientSearchRecordQuery } from '../../types/client';
+/**
+ * 获取客户端查询列表
+ * @param bizId 业务ID
+ * @param appId 应用ID
+ * @returns
+ */
+export const getClientQueryList = (bizId: string, appId: number, query: IClinetCommonQuery) =>
+  http.post(`/config/biz/${bizId}/apps/${appId}/clients`, query);
+
+/**
+ * 获取客户端配置拉取记录
+ * @param bizId 业务ID
+ * @param appId 应用ID
+ * @param clientId 客户端id
+ * @returns
+ */
+export const getClientPullRecord = (bizId: string, appId: number, clientId: number, query: IClinetCommonQuery) =>
+  http.post(`/config/biz/${bizId}/apps/${appId}/client_events/${clientId}`, query);
+
+/**
+ * 获取客户端搜索记录
+ * @param bizId 业务ID
+ * @param appId 应用ID
+ * @param clientId 客户端id
+ * @returns
+ */
+export const getClientSearchRecord = (bizId: string, appId: number, params: IClinetCommonQuery) =>
+  http.get(`/config/biz/${bizId}/apps/${appId}/client_querys`, { params });
+
+/**
+ * 新增客户端搜索记录
+ * @param bizId 业务ID
+ * @param appId 应用ID
+ * @param clientId 客户端id
+ * @returns
+ */
+export const createClientSearchRecord = (bizId: string, appId: number, query: ICreateClientSearchRecordQuery) =>
+  http.post(`config/biz/${bizId}/apps/${appId}/client_querys`, query);
+
+/**
+ * 编辑客户端搜索记录
+ * @param bizId 业务ID
+ * @param appId 应用ID
+ * @param clientId 客户端id
+ * @returns
+ */
+export const updateClientSearchRecord = (
+  bizId: string,
+  appId: number,
+  recordId: number,
+  query: ICreateClientSearchRecordQuery,
+) => http.put(`/config/biz/${bizId}/apps/${appId}/client_querys/${recordId}`, query);
+
+/**
+ * 删除客户端搜索记录
+ * @param bizId 业务ID
+ * @param appId 应用ID
+ * @param clientId 客户端id
+ * @returns
+ */
+export const deleteClientSearchRecord = (bizId: string, appId: number, recordId: number) =>
+  http.delete(`config/biz/${bizId}/apps/${appId}/client_querys/${recordId}`);
---
+++ bcs-services/bcs-bscp/ui/src/constants/client.ts
@@ -0,0 +1,118 @@
+export const CLIENT_SEARCH_DATA = [
+  {
+    name: 'UID',
+    value: 'uid',
+  },
+  {
+    name: 'IP',
+    value: 'ip',
+  },
+  {
+    name: '标签',
+    value: 'label',
+  },
+  {
+    name: '当前配置版本',
+    value: 'current_release_name',
+  },
+  {
+    name: '目标配置版本',
+    value: 'target_release_name',
+  },
+  {
+    name: '最近一次拉取配置状态',
+    value: 'release_change_status',
+    children: [
+      {
+        name: '成功',
+        value: 'Success',
+      },
+      {
+        name: '失败',
+        value: 'Failed',
+      },
+      {
+        name: '处理中',
+        value: 'Processing',
+      },
+      {
+        name: '跳过',
+        value: 'Skip',
+      },
+    ],
+  },
+  // {
+  //   name: '附加信息',
+  //   value: 'annotations',
+  // },
+  {
+    name: '在线状态',
+    value: 'online_status',
+    children: [
+      {
+        name: '在线',
+        value: 'online',
+      },
+      {
+        name: '未在线',
+        value: 'offline',
+      },
+    ],
+  },
+  {
+    name: '客户端组件版本',
+    value: 'client_version',
+  },
+];
+
+export const CLIENT_STATUS_MAP = {
+  Success: '成功',
+  Failed: '失败',
+  Processing: '拉取中',
+  Skip: '跳过',
+  online: '在线',
+  offline: '离线',
+};
+
+export const CLIENT_HEARTBEAT_LIST = [
+  {
+    value: 1,
+    label: '近1分钟',
+  },
+  {
+    value: 5,
+    label: '近5分钟',
+  },
+  {
+    value: 60,
+    label: '近1小时',
+  },
+  {
+    value: 360,
+    label: '近6小时',
+  },
+  {
+    value: 720,
+    label: '近12小时',
+  },
+  {
+    value: 1440,
+    label: '近1天',
+  },
+  {
+    value: 4320,
+    label: '近3天',
+  },
+  {
+    value: 10080,
+    label: '近7天',
+  },
+  {
+    value: 20160,
+    label: '近15天',
+  },
+  {
+    value: 43200,
+    label: '近30天',
+  },
+];
