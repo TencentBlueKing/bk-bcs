071c06167b0ea7c222cb4f4c8ea6fa5754f4c901
dove0012 <dove0012@163.com>
2024-03-27 16:06:07 +0800

    添加导入华为云集群功能

---
+++ bcs-services/bcs-cluster-manager/internal/cloudprovider/huawei/taskmgr.go
@@ -0,0 +1,248 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package huawei xxx
+package huawei
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+
+	proto "github.com/Tencent/bk-bcs/bcs-services/bcs-cluster-manager/api/clustermanager"
+	"github.com/Tencent/bk-bcs/bcs-services/bcs-cluster-manager/internal/cloudprovider"
+	"github.com/Tencent/bk-bcs/bcs-services/bcs-cluster-manager/internal/cloudprovider/huawei/tasks"
+)
+
+var taskMgr sync.Once
+
+func init() {
+	taskMgr.Do(func() {
+		cloudprovider.InitTaskManager(cloudName, newtask())
+	})
+}
+
+func newtask() *Task {
+	task := &Task{
+		works: make(map[string]interface{}),
+	}
+
+	// import task
+	task.works[importClusterNodesStep.StepMethod] = tasks.ImportClusterNodesTask
+	task.works[registerClusterKubeConfigStep.StepMethod] = tasks.RegisterClusterKubeConfigTask
+
+	return task
+}
+
+// Task task mgr
+type Task struct {
+	works map[string]interface{}
+}
+
+// Name get cloudName
+func (t *Task) Name() string {
+	return cloudName
+}
+
+// GetAllTask register all backgroup task for worker running
+func (t *Task) GetAllTask() map[string]interface{} {
+	return t.works
+}
+
+// BuildCreateClusterTask build create cluster task
+func (t *Task) BuildCreateClusterTask(cls *proto.Cluster, opt *cloudprovider.CreateClusterOption) (
+	*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildImportClusterTask build import cluster task
+func (t *Task) BuildImportClusterTask(cls *proto.Cluster, opt *cloudprovider.ImportClusterOption) (
+	*proto.Task, error) {
+	// validate request params
+	if cls == nil {
+		return nil, fmt.Errorf("BuildImportClusterTask cluster info empty")
+	}
+	if opt == nil || opt.Cloud == nil {
+		return nil, fmt.Errorf("BuildImportClusterTask TaskOptions is lost")
+	}
+
+	// init task information
+	nowStr := time.Now().Format(time.RFC3339)
+	task := &proto.Task{
+		TaskID:         uuid.New().String(),
+		TaskType:       cloudprovider.GetTaskType(cloudName, cloudprovider.ImportCluster),
+		TaskName:       "纳管CCE集群",
+		Status:         cloudprovider.TaskStatusInit,
+		Message:        "task initializing",
+		Start:          nowStr,
+		Steps:          make(map[string]*proto.Step),
+		StepSequence:   make([]string, 0),
+		ClusterID:      cls.ClusterID,
+		ProjectID:      cls.ProjectID,
+		Creator:        opt.Operator,
+		Updater:        opt.Operator,
+		LastUpdate:     nowStr,
+		CommonParams:   make(map[string]string),
+		ForceTerminate: false,
+	}
+
+	// generate taskName
+	taskName := fmt.Sprintf(importClusterTaskTemplate, cls.ClusterID)
+	task.CommonParams[cloudprovider.TaskNameKey.String()] = taskName
+
+	// setting all steps details
+	importCluster := &ImportClusterTaskOption{Cluster: cls}
+	// step1: import cluster registerKubeConfigStep
+	importCluster.BuildRegisterKubeConfigStep(task)
+	// step2: import cluster nodes step
+	importCluster.BuildImportClusterNodesStep(task)
+
+	// set current step
+	if len(task.StepSequence) == 0 {
+		return nil, fmt.Errorf("BuildImportClusterTask task StepSequence empty")
+	}
+	task.CurrentStep = task.StepSequence[0]
+	task.CommonParams[cloudprovider.OperatorKey.String()] = opt.Operator
+	task.CommonParams[cloudprovider.JobTypeKey.String()] = cloudprovider.ImportClusterJob.String()
+
+	return task, nil
+}
+
+// BuildAddExternalNodeToCluster add external to cluster
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildAddExternalNodeToCluster(group *proto.NodeGroup, nodes []*proto.Node, // nolint
+	opt *cloudprovider.AddExternalNodesOption) (*proto.Task, error) {
+	// AddExternalNodeToCluster has three steps:
+	// 1. call qcloud getExternalNodeScript get addNodes script
+	// 2. call bksops add nodes to cluster
+	// may be need to call external previous or behind operation by bkops
+
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildSwitchAsOptionStatusTask build switch auto scaler option status task - 开启/关闭集群自动扩缩容
+func (t *Task) BuildSwitchAsOptionStatusTask(scalingOption *proto.ClusterAutoScalingOption, enable bool,
+	opt *cloudprovider.CommonOption) (*proto.Task, error) {
+	return nil, nil
+}
+
+// BuildUpdateNodeGroupTask when update nodegroup, we need to create background task,
+func (t *Task) BuildUpdateNodeGroupTask(group *proto.NodeGroup, opt *cloudprovider.CommonOption) (*proto.Task, error) {
+	return nil, nil
+}
+
+// BuildDeleteExternalNodeFromCluster remove external node from cluster
+func (t *Task) BuildDeleteExternalNodeFromCluster(group *proto.NodeGroup, nodes []*proto.Node,
+	opt *cloudprovider.DeleteExternalNodesOption) (*proto.Task, error) {
+	// DeleteExternalNodeFromCluster has two steps:
+	// 1. call qcloud DeleteExternalNodes
+	// 2. call bksops clean node
+	// may be need to call external previous or behind operation by bkops
+
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildCreateVirtualClusterTask build create virtual cluster task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildCreateVirtualClusterTask(cls *proto.Cluster,
+	opt *cloudprovider.CreateVirtualClusterOption) (*proto.Task, error) {
+	return nil, nil
+}
+
+// BuildDeleteVirtualClusterTask build delete virtual cluster task
+func (t *Task) BuildDeleteVirtualClusterTask(cls *proto.Cluster,
+	opt *cloudprovider.DeleteVirtualClusterOption) (*proto.Task, error) {
+	return nil, nil
+}
+
+// BuildDeleteClusterTask build deleteCluster task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildDeleteClusterTask(cls *proto.Cluster, opt *cloudprovider.DeleteClusterOption) (
+	*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildAddNodesToClusterTask build addNodes task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildAddNodesToClusterTask(cls *proto.Cluster, nodes []*proto.Node,
+	opt *cloudprovider.AddNodesOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildRemoveNodesFromClusterTask build removeNodes task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildRemoveNodesFromClusterTask(cls *proto.Cluster, nodes []*proto.Node,
+	opt *cloudprovider.DeleteNodesOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildCreateNodeGroupTask build create node group task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildCreateNodeGroupTask(group *proto.NodeGroup, opt *cloudprovider.CreateNodeGroupOption) (
+	*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildCleanNodesInGroupTask clean specified nodes in NodeGroup
+// including remove nodes from NodeGroup, clean data in nodes
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildCleanNodesInGroupTask(nodes []*proto.Node, group *proto.NodeGroup,
+	opt *cloudprovider.CleanNodesOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildDeleteNodeGroupTask when delete nodegroup, we need to create background
+// task to clean all nodes in nodegroup, release all resource in cloudprovider,
+// finnally delete nodes information in local storage.
+// @param group: need to delete
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildDeleteNodeGroupTask(group *proto.NodeGroup, nodes []*proto.Node,
+	opt *cloudprovider.DeleteNodeGroupOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildMoveNodesToGroupTask build move nodes to group task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildMoveNodesToGroupTask(nodes []*proto.Node, group *proto.NodeGroup,
+	opt *cloudprovider.MoveNodesOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildUpdateDesiredNodesTask build update desired nodes task
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildUpdateDesiredNodesTask(desired uint32, group *proto.NodeGroup,
+	opt *cloudprovider.UpdateDesiredNodeOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildSwitchNodeGroupAutoScalingTask ensure auto scaler status and update nodegroup status to normal
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildSwitchNodeGroupAutoScalingTask(group *proto.NodeGroup, enable bool,
+	opt *cloudprovider.SwitchNodeGroupAutoScalingOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildUpdateAutoScalingOptionTask update auto scaling option
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildUpdateAutoScalingOptionTask(scalingOption *proto.ClusterAutoScalingOption,
+	opt *cloudprovider.UpdateScalingOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
+
+// BuildSwitchAutoScalingOptionStatusTask switch auto scaling option status
+// NOCC:CCN_threshold(工具误报:),golint/fnsize(设计如此:)
+func (t *Task) BuildSwitchAutoScalingOptionStatusTask(scalingOption *proto.ClusterAutoScalingOption, enable bool,
+	opt *cloudprovider.CommonOption) (*proto.Task, error) {
+	return nil, cloudprovider.ErrCloudNotImplemented
+}
