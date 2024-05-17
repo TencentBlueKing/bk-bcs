ccb8a6278ccd371bfff587bda3a24cebbc1a6121
johnypeng <johnypeng@canway.net>
2024-05-17 19:54:18 +0800

    feat: bscp supports file multiple match (#3185)
    * feat: bscp supports file multiple match
    
    * feat: optimize loop logic

--- bcs-services/bcs-bscp/cmd/feed-server/service/rpc_sidecar.go
+++ bcs-services/bcs-bscp/cmd/feed-server/service/rpc_sidecar.go
@@ -23,6 +23,7 @@ import (
 	"time"
 
 	prm "github.com/prometheus/client_golang/prometheus"
+	"github.com/samber/lo"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -355,11 +356,22 @@ func (s *Service) PullAppFileMeta(ctx context.Context, req *pbfs.PullAppFileMeta
 	}
 
 	fileMetas := make([]*pbfs.FileMeta, 0, len(metas.ConfigItems))
+	// 支持req.Match多个匹配，且兼容原有req.Key单个匹配，当req.Match和req.Key都未设置时则获取全部配置项
+	match := req.Match
+	if req.Key != "" {
+		match = append(match, req.Key)
+	}
 	for _, ci := range metas.ConfigItems {
-		ok, _ := tools.MatchConfigItem(req.Key, ci.ConfigItemSpec.Path, ci.ConfigItemSpec.Name)
-		if req.Key != "" && !ok {
-			continue
+		if len(match) > 0 {
+			isMatch := lo.Filter(match, func(scope string, _ int) bool {
+				ok, _ := tools.MatchConfigItem(scope, ci.ConfigItemSpec.Path, ci.ConfigItemSpec.Name)
+				return ok
+			})
+			if len(isMatch) == 0 {
+				continue
+			}
 		}
+
 		app, err := s.bll.AppCache().GetMeta(im.Kit, req.BizId, ci.ConfigItemAttachment.AppId)
 		if err != nil {
 			return nil, status.Errorf(codes.Aborted, "get app meta failed, %s", err.Error())
