3e28fc21a44bbfcfc9e953428721e478a1759f1b
Yuikill <1191184301@qq.com>
2024-06-12 16:30:36 +0800

    feat: 客户端密钥支持密钥搜索

--- bcs-services/bcs-bscp/ui/src/i18n/en-us.ts
+++ bcs-services/bcs-bscp/ui/src/i18n/en-us.ts
@@ -630,7 +630,7 @@ export default {
   '密钥仅用于 SDK/API 拉取配置使用。服务管理/配置管理/分组管理等功能的权限申请，请前往': 'The key is only used for SDK/API pull configurations. To apply for permissions for functions such as service management/configuration management/group management, please go to',
   蓝鲸权限中心: 'Blue Whale Authority Center',
   新建密钥: 'New Credentials',
-  '密钥名称/说明/关联规则/更新人': 'Key name/ Description/ Association rules/ Updater',
+  '密钥名称/密钥/说明/关联规则/更新人': 'Key name/ Key/ Description/ Association rules/ Updater',
   密钥名称: 'Key name',
   密钥名称支持中英文: 'Key name supports Chinese and English',
   密钥: 'Credentials',
