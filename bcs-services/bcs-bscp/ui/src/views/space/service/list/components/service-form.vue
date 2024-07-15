d7e4ad0d5bf01a974e800b365c5cb195b2d80b9a
Yuikill <105910874+Yuikill@users.noreply.github.com>
2024-07-15 11:48:32 +0800

    fix: 修复新建文本配置项报错 (#3364)
    * fix: 修复新建文本配置项报错
    
    * fix: 国际化内容修复
    
    * fix: 调整单文件大小参数

--- bcs-services/bcs-bscp/ui/src/views/space/service/list/components/service-form.vue
+++ bcs-services/bcs-bscp/ui/src/views/space/service/list/components/service-form.vue
@@ -5,13 +5,21 @@
         v-model="localData.name"
         :placeholder="t('请输入2-32字符，只允许英文、数字、下划线、中划线且必须以英文、数字开头和结尾')"
         :disabled="editable"
-        @input="handleChange" />
+        @input="handleChange"
+        v-bk-tooltips="{
+          content: t('请输入2-32字符，只允许英文、数字、下划线、中划线且必须以英文、数字开头和结尾'),
+          disabled: locale === 'zh-cn',
+        }" />
     </bk-form-item>
     <bk-form-item :label="t('form_服务别名')" property="alias" required>
       <bk-input
         v-model="localData.alias"
         :placeholder="t('请输入2-128字符，只允许中文、英文、数字、下划线、中划线且必须以中文、英文、数字开头和结尾')"
-        @input="handleChange" />
+        @input="handleChange"
+        v-bk-tooltips="{
+          content: t('请输入2-128字符，只允许中文、英文、数字、下划线、中划线且必须以中文、英文、数字开头和结尾'),
+          disabled: locale === 'zh-cn',
+        }" />
     </bk-form-item>
     <bk-form-item :label="t('服务描述')" property="memo">
       <bk-input
@@ -48,7 +56,7 @@
   import { IServiceEditForm } from '../../../../../../types/service';
   import { CONFIG_KV_TYPE } from '../../../../../constants/config';
 
-  const { t } = useI18n();
+  const { t, locale } = useI18n();
 
   const emits = defineEmits(['change']);
 
