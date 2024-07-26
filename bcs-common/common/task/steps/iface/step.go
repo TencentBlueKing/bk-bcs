27f142a9d75cd763a36094f7a3eefd0c2d13cdf4
ifooth <thezero12@hotmail.com>
2024-07-27 00:38:34 +0800

    feat: add storage interface

--- bcs-common/common/task/steps/iface/step.go
+++ bcs-common/common/task/steps/iface/step.go
@@ -19,16 +19,6 @@ import (
 // If the returned error is SkipRetry or an error wraps SkipRetry, retry is
 // skipped and the task will be immediately archived instead.
 type Step interface {
+	Name() string
 	Run(context.Context, *types.Step) error
-}
-
-// The StepFunc type is an adapter to allow the use of
-// ordinary functions as a Step. If f is a function
-// with the appropriate signature, StepFunc(f) is a
-// Step that calls f.
-type StepFunc func(context.Context, *types.Step) error
-
-// Run calls fn(ctx, task)
-func (fn StepFunc) Run(ctx context.Context, task *types.Step) error {
-	return fn(ctx, task)
 }
