52028e6c24041e308d08ef6b52ec74cccf7d0bf3
Yuikill <105910874+Yuikill@users.noreply.github.com>
2024-09-09 10:40:55 +0800

    fix: 配置文件路径限制优化 (#3500)

--- bcs-services/bcs-bscp/ui/src/views/space/service/detail/config/config-list/config-table-list/config-form.vue
+++ bcs-services/bcs-bscp/ui/src/views/space/service/detail/config/config-list/config-table-list/config-form.vue
@@ -228,33 +228,7 @@
     // 配置文件绝对路径校验规则，path+filename
     fileAP: [
       {
-        validator: (val: string) => {
-          // 必须为绝对路径, 且不能以/结尾
-          if (!val.startsWith('/') || val.endsWith('/')) {
-            return false;
-          }
-
-          const parts = val.split('/').slice(1);
-          const fileName = parts.pop() as string;
-
-          // 文件名称校验
-          // 文件名和路径不能全由.组成
-          if (!/^((?!\.{1,}$)[\u4e00-\u9fa5A-Za-z0-9.\-_#%,:?!@$^+=\\[\]{}]+)$/.test(fileName)) {
-            return false;
-          }
-
-          let isValid = true;
-          // 文件路径校验
-          parts.some((part) => {
-            if (!/^((?!\.{1,}$)[\u4e00-\u9fa5A-Za-z0-9.\-_#%,:?!@$^+=\\[\]{}]+)$/.test(part)) {
-              isValid = false;
-              return true;
-            }
-            return false;
-          });
-
-          return isValid;
-        },
+        validator: (val: string) => /^\/(?:[^/]+\/)*[^/]+$/.test(val),
         message: t('无效的路径,路径不符合Unix文件路径格式规范'),
         trigger: 'change',
       },
