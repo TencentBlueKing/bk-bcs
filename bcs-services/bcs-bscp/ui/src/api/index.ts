06251ee68bb5468eece7f992cca70d58ee04bf49
q15971095971 <764419406@qq.com>
2024-09-30 11:08:25 +0800

    feat: 适配后端人员地址注入变量更新--story=115998324

--- bcs-services/bcs-bscp/ui/src/api/index.ts
+++ bcs-services/bcs-bscp/ui/src/api/index.ts
@@ -120,5 +120,5 @@ export const loginOut = () =>
  * @returns
  */
 export const approverListApi = () =>
-  `${(window as any).User_Man_Host}/api/c/compapi/v2/usermanage/fs_list_users/?app_code=bk-magicbox&page_size=1000&page=1`;
+  `${(window as any).USER_MAN_HOST}/api/c/compapi/v2/usermanage/fs_list_users/?app_code=bk-magicbox&page_size=1000&page=1`;
 // /api/c/compapi/v2/usermanage/fs_list_users/?app_code=bk-magicbox&page_size=1000&page=1"
