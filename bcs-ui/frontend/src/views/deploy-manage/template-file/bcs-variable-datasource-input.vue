11f2f47c2ad87c0b4b995dcd40afd5291d013ba4
v_hyfahuang <v_hyfahuang@tencent.com>
2024-10-17 07:32:10 +0000

    feat: --story=120108898 【UI】完善BcsVarInput组件，支持datasource数据源 # Reviewed, transaction id: 20968 (merge request !2065)
    Squash merge branch 'feat/enhance-bcsvarinput-support-datasource' into 'master'
    feat: --story=120108898 【UI】完善BcsVarInput组件，支持datasource数据源
    # Reviewed, transaction id: 20968
    
     
    
    
    
    TAPD: --story=120108898

--- bcs-ui/frontend/src/views/deploy-manage/template-file/bcs-variable-datasource-input.vue
+++ bcs-ui/frontend/src/views/deploy-manage/template-file/bcs-variable-datasource-input.vue
@@ -11,7 +11,7 @@
       <ul class="max-h-[360px] overflow-auto">
         <li
           class="bcs-dropdown-item"
-          v-for="item in fileStore.varList"
+          v-for="item in newDatasource"
           :key="item.id"
           @click="handleSetVar(item.key)">
           <span>{{ `\{\{ ${item.key} \}\}` }}</span>
@@ -22,16 +22,20 @@
   </PopoverSelector>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import { store as fileStore } from './use-store';
 
 import PopoverSelector from '@/components/popover-selector.vue';
-
+type DataSourceItem = {
+  label: string;
+  value: string;
+}
 interface Props {
   value?: string | number
   disableVarList?: boolean
   type?: string
+  datasource?: DataSourceItem[]
 }
 type Emits = (e: 'input', v: string|number) => void;
 
@@ -39,6 +43,7 @@ const props = withDefaults(defineProps<Props>(), {
   value: () => '',
   disableVarList: () => false,
   type: () => 'text',
+  datasource: () => []
 });
 const emits = defineEmits<Emits>();
 
@@ -53,6 +58,14 @@ function handleInput(v: string) {
     popoverRef.value?.show();
   }
 }
+const newDatasource = computed(() => props.datasource?.length
+  ? props.datasource.map(item => {
+      return {
+        key: item.value,
+        name: item.label
+      };
+    })
+  : fileStore.varList);
 
 function handleSetVar(key: string) {
   emits('input', `{{ ${key} }}`);
