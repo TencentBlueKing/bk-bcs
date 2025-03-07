ae5002c574a374e32b330548594f2abdc912976f
v_chxuchen <v_chxuchen@tencent.com>
2025-03-07 10:53:20 +0000

    fix: fix sharedproject api response data name (merge request !2251)
    Squash merge branch 'v1.30.x' into 'v1.30.x'
    fix: [ClusterManagement] fix sharedproject api response data name
    
    修复了sharedproject API响应数据名称的错误
    proto-gen-go 从 v1.25.0 升级最新到 v1.26.0 并同步更新 proto 文件

--- bcs-services/bcs-cluster-manager/api/clustermanager/clustermanager.pb.validate.go
+++ bcs-services/bcs-cluster-manager/api/clustermanager/clustermanager.pb.validate.go
@@ -67045,11 +67045,11 @@ func (m *GetClusterSharedProjectResponse) validate(all bool) error {
 	// no validation rules for Result
 
 	if all {
-		switch v := interface{}(m.GetSharedProjects()).(type) {
+		switch v := interface{}(m.GetData()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
 				errors = append(errors, GetClusterSharedProjectResponseValidationError{
-					field:  "SharedProjects",
+					field:  "Data",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
@@ -67057,16 +67057,16 @@ func (m *GetClusterSharedProjectResponse) validate(all bool) error {
 		case interface{ Validate() error }:
 			if err := v.Validate(); err != nil {
 				errors = append(errors, GetClusterSharedProjectResponseValidationError{
-					field:  "SharedProjects",
+					field:  "Data",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
 			}
 		}
-	} else if v, ok := interface{}(m.GetSharedProjects()).(interface{ Validate() error }); ok {
+	} else if v, ok := interface{}(m.GetData()).(interface{ Validate() error }); ok {
 		if err := v.Validate(); err != nil {
 			return GetClusterSharedProjectResponseValidationError{
-				field:  "SharedProjects",
+				field:  "Data",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
