965d4a79f5380a40ae22932dcbacf60bf5e73f4a
zhenlinwen <zhenlinwen@tencent.com>
2025-06-05 08:03:47 +0000

    --story=124475911 【bcs-mesh-manager】服务基础框架搭建 (merge request !2380)
    Squash merge branch 'f-bcs-mesh-manager' into 'master'
    --story=124475911 【bcs-mesh-manager】服务基础框架搭建
    
     
    
    
    
    TAPD: --story=124475911

---
+++ bcs-services/bcs-mesh-manager/cmd/mesh-manager/main.go
@@ -0,0 +1,42 @@
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package main 提供mesh manager的入口
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Tencent/bk-bcs/bcs-common/common/blog"
+
+	"github.com/Tencent/bk-bcs/bcs-services/bcs-mesh-manager/cmd/mesh-manager/app"
+)
+
+func main() {
+	opts := app.NewMeshManagerOptions()
+	if err := app.Parse(opts); err != nil {
+		fmt.Fprintf(os.Stderr, "set config file failed, err %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	blog.InitLogs(opts.LogConfig)
+	defer blog.CloseLogs()
+
+	server := app.NewServer(opts)
+	if err := server.Init(); err != nil {
+		blog.Fatalf("init mesh manager failed, err %s", err.Error())
+	}
+	if err := server.Run(); err != nil {
+		blog.Fatalf("run mesh manager failed, %s", err.Error())
+	}
+}
