03682d56814873be612f78614f8dc26037481ddd
evanxinli <evanxinli@tencent.com>
2026-01-21 07:37:18 +0000

    --story=130204409 【集群管理】修复腾讯公有云上架节点问题 (merge request !2687)
    Squash merge branch 'bcs-cluster-manager-20260121' into 'master'
    --story=130204409 【集群管理】修复腾讯公有云上架节点问题
    
     
    
    
    
    TAPD: --story=130204409
    TAPD: --story=120703235

--- bcs-services/bcs-cluster-manager/internal/actions/cluster/update.go
+++ bcs-services/bcs-cluster-manager/internal/actions/cluster/update.go
@@ -581,6 +581,9 @@ func (ua *AddNodesAction) addNodesToCluster() error {
 			if len(ua.req.Login.GetKeyPair().GetKeyPublic()) > 0 {
 				loginInfo.KeyPair.KeyPublic, _ = encrypt.Encrypt(nil, ua.req.Login.GetKeyPair().GetKeyPublic())
 			}
+			if len(ua.req.Login.GetKeyPair().GetKeyID()) > 0 {
+				loginInfo.KeyPair.KeyID = ua.req.Login.GetKeyPair().GetKeyID()
+			}
 			return loginInfo
 		}(),
 		Advance:      ua.req.GetAdvance(),
