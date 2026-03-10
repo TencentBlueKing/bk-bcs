71537b36921cdb3a1e5236656609ed67be5ef5fc
porterlin <porterlin@tencent.com>
2026-03-10 06:34:14 +0000

    --story=131407139 [bcs-ingress-controller] 问题修复 (merge request !2727)
    Squash merge branch 'porterlin-dev4' into 'master'
    --story=131407139 [bcs-ingress-controller] 问题修复
    
     
    
    
    
    TAPD: --story=131407139

--- bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/webhookserver/mutating_ingress.go
+++ bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/webhookserver/mutating_ingress.go
@@ -13,13 +13,10 @@
 package webhookserver
 
 import (
-	"strings"
-
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/admission/v1"
 
 	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/cloud"
-	"github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/bcs-network/bcs-ingress-controller/internal/constant"
 	networkextensionv1 "github.com/Tencent/bk-bcs/bcs-runtime/bcs-k8s/kubernetes/apis/networkextension/v1"
 )
 
@@ -34,19 +31,13 @@ func (s *Server) mutateIngress(ingress *networkextensionv1.Ingress, operation v1
 		return nil, errors.New(msg)
 	}
 
-	// 对于可以通过修改ingress以外配置使ingress正常运行的错误，返回warning
-	// k8s 1.18之前不支持warning，暂时使用annotation方式
-	// ingress controller reconcile过程中会更新该annotation
-	var warnings []string
-	warnings = append(warnings, s.ingressConverter.CheckIngressServiceAvailable(ingress)...)
-
 	_, err := s.ingressConverter.GetIngressLoadBalancers(ingress)
 	if err != nil {
 		// 避免lb被删除后导致ingress无法正常更新
 		if operation == v1.Update && err == cloud.ErrLoadbalancerNotFound {
 			return nil, nil
 		}
-		warnings = append(warnings, err.Error())
+		return nil, err
 	}
 
 	err = s.conflictHandler.IsIngressConflict(ingress)
@@ -54,25 +45,5 @@ func (s *Server) mutateIngress(ingress *networkextensionv1.Ingress, operation v1
 		return nil, err
 	}
 
-	var retPatches []PatchOperation
-	annotationWarningPatch := s.generateWarningAnnotationPatch(ingress, warnings)
-	retPatches = append(retPatches, annotationWarningPatch)
-
-	return retPatches, nil
-}
-
-// generate warning annotations
-func (s *Server) generateWarningAnnotationPatch(ingress *networkextensionv1.Ingress, warnings []string) PatchOperation {
-	annotations := ingress.Annotations
-	op := constant.PatchOperationReplace
-	if len(annotations) == 0 {
-		op = constant.PatchOperationAdd
-		annotations = make(map[string]string)
-	}
-	annotations[networkextensionv1.AnnotationKeyForWarnings] = strings.Join(warnings, ";")
-	return PatchOperation{
-		Path:  constant.PatchPathPodAnnotations,
-		Op:    op,
-		Value: annotations,
-	}
+	return nil, nil
 }
