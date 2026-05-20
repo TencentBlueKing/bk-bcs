90bf52355d9b8d69b4d78cf525bbb36faebb178f
joeecai <joeecai@tencent.com>
2026-05-20 09:50:51 +0000

    --story=134457974 安全问题修复 (merge request !2794)
    Squash merge branch 'fix/sec-1.30' into 'v1.30.x'
    --story=134457974 安全问题修复
    
     
    
    
    
    TAPD: --story=134457974

--- bcs-services/bcs-storage/pkg/handler/bcs-storage.go
+++ bcs-services/bcs-storage/pkg/handler/bcs-storage.go
@@ -1465,8 +1465,6 @@ func (s *Storage) GetSecretK8S(ctx context.Context, req *storage.SecretK8SReques
 		rsp.Code = common.AdditionErrorCode + 500
 		return nil
 	}
-	// 打印请求体
-	blog.Infof("GetSecretK8S req: %v", util.PrettyStruct(req))
 
 	data, err := dynamicquery.HandlerSecretK8SRequest(ctx, req)
 	if err != nil {
@@ -1487,9 +1485,6 @@ func (s *Storage) GetSecretK8S(ctx context.Context, req *storage.SecretK8SReques
 	rsp.Result = true
 	rsp.Code = common.BcsSuccess
 	rsp.Message = common.BcsSuccessStr
-
-	// 打印响应体
-	blog.Infof("GetSecretK8S rsp: %v", util.PrettyStruct(rsp))
 	return nil
 }
 
@@ -1503,8 +1498,6 @@ func (s *Storage) PostSecretK8S(ctx context.Context, req *storage.SecretK8SReque
 		rsp.Code = common.AdditionErrorCode + 500
 		return nil
 	}
-	// 打印请求体
-	blog.Infof("PostSecretK8S req: %v", util.PrettyStruct(req))
 
 	data, err := dynamicquery.HandlerSecretK8SRequest(ctx, req)
 	if err != nil {
@@ -1525,9 +1518,6 @@ func (s *Storage) PostSecretK8S(ctx context.Context, req *storage.SecretK8SReque
 	rsp.Result = true
 	rsp.Code = common.BcsSuccess
 	rsp.Message = common.BcsSuccessStr
-
-	// 打印响应体
-	blog.Infof("PostSecretK8S rsp: %v", util.PrettyStruct(rsp))
 	return nil
 }
 
