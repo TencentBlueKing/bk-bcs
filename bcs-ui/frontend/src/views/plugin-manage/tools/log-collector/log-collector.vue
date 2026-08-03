573becdaa8781e364d5dc222e80e640cafe9c5ba
adevjoe <adevjoe@gmail.com>
2026-08-03 16:48:33 +0800

    fix: log-collector import

--- bcs-ui/frontend/src/views/plugin-manage/tools/log-collector/log-collector.vue
+++ bcs-ui/frontend/src/views/plugin-manage/tools/log-collector/log-collector.vue
@@ -392,6 +392,7 @@ import useLog, { IClusterGroup, IRuleData } from './use-log';
 
 import $bkMessage from '@/common/bkmagic';
 import { LOG_COLLECTOR } from '@/common/constant';
+import { formatTimeWithTimezone } from '@/common/util';
 import $bkInfo from '@/components/bk-magic-2.0/bk-info';
 import ClusterSelect from '@/components/cluster-selector/cluster-select.vue';
 import ContentHeader from '@/components/layout/Header.vue';
